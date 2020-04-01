90fd4e96a619fa72addbcf6bec13e9c96ba90565
Bastian Müller <bastian@axiomzen.co>
2020-04-01 15:32:58 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,33 @@
+<#
+.Synopsis
+    Install the Flow CLI on Windows.
+.DESCRIPTION
+    By default, the latest release will be installed.
+    If '-Version' is specified, then the given version is installed.
+.Parameter Directory
+    The destination path to install to.
+.Parameter Version
+    The version to install.
+.Parameter AddToPath
+    Add the absolute destination path to the 'User' scope environment variable 'Path'.
+.EXAMPLE
+    Install the current version
+    .\install.ps1
+.EXAMPLE
+    Install version v0.5.2
+    .\install.ps1 -Version v0.5.2
+.EXAMPLE
+    Invoke-Expression "& { $(Invoke-RestMethod 'https://storage.googleapis.com/flow-cli/install.ps1') }"
+#>
+param (
+    [string] $version="",
+    [string] $directory = "$env:APPDATA\Flow",
+    [bool] $addToPath = $true
+)
 
-param ($version="", $directory = "$env:APPDATA\Flow")
+Set-StrictMode -Version 3.0
+
+$ErrorActionPreference = "Stop"
 
 $baseURL = "https://storage.googleapis.com/flow-cli"
 
@@ -15,9 +43,12 @@ $progressPreference = 'silentlyContinue'
 
 Invoke-WebRequest -Uri "$baseURL/flow-x86_64-windows-$version" -UseBasicParsing -OutFile "$directory\flow.exe"
 
-$newPath = $Env:Path + ";$directory"
-[System.Environment]::SetEnvironmentVariable("PATH", $newPath)
-[System.Environment]::SetEnvironmentVariable("PATH", $newPath, [System.EnvironmentVariableTarget]::User)
+if ($addToPath) {
+    Write-Output "Adding to PATH ..."
+    $newPath = $Env:Path + ";$directory"
+    [System.Environment]::SetEnvironmentVariable("PATH", $newPath)
+    [System.Environment]::SetEnvironmentVariable("PATH", $newPath, [System.EnvironmentVariableTarget]::User)
+}
 
 Write-Output "Done."
 
