66da20473ba2bc0885b933c7e63bf74090ddddc3
Bastian Müller <bastian@axiomzen.co>
2020-04-17 08:40:43 -0700

    use new SDK API for transaction sending and account creation

--- flow/transactions/send/send.go
+++ flow/transactions/send/send.go
@@ -1,16 +1,11 @@
 package send
 
 import (
-    "context"
 	"io/ioutil"
 	"log"
 
 	"github.com/psiemens/sconfig"
 	"github.com/spf13/cobra"
-
-	"github.com/dapperlabs/flow-go-sdk"
-	"github.com/dapperlabs/flow-go-sdk/client"
-	"github.com/dapperlabs/flow-go-sdk/keys"
 
 	cli "github.com/dapperlabs/flow-cli/flow"
 )
@@ -18,7 +13,6 @@ import (
 type Config struct {
 	Signer string `default:"root" flag:"signer,s"`
 	Code   string `flag:"code,c"`
-	Nonce  uint64 `flag:"nonce,n"`
 	Host   string `default:"127.0.0.1:3569" flag:"host" info:"Flow Observation API host address"`
 }
 
@@ -30,7 +24,7 @@ var Cmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		projectConf := cli.LoadConfig()
 
-		signer := projectConf.Accounts[conf.Signer]
+		signerAccount := projectConf.Accounts[conf.Signer]
 
 		var (
 			code []byte
@@ -40,34 +34,11 @@ var Cmd = &cobra.Command{
 		if conf.Code != "" {
 			code, err = ioutil.ReadFile(conf.Code)
 			if err != nil {
-				cli.Exitf(1, "Failed to load BPL code from %s", conf.Code)
+				cli.Exitf(1, "Failed to read transaction script from %s", conf.Code)
 			}
 		}
 
-		tx := flow.Transaction{
-			Script:         code,
-			Nonce:          conf.Nonce,
-			ComputeLimit:   10,
-			PayerAccount:   signer.Address,
-			ScriptAccounts: []flow.Address{signer.Address},
-		}
-
-		sig, err := keys.SignTransaction(tx, signer.PrivateKey)
-		if err != nil {
-			cli.Exit(1, "Failed to sign transaction")
-		}
-
-		tx.AddSignature(signer.Address, sig)
-
-		client, err := client.New(conf.Host)
-		if err != nil {
-			cli.Exit(1, "Failed to connect to emulator")
-		}
-
-		err = client.SendTransaction(context.Background(), tx)
-		if err != nil {
-			cli.Exitf(1, "Failed to send transaction: %v", err)
-		}
+		cli.SendTransaction(conf.Host, signerAccount, code)
 	},
 }
 
