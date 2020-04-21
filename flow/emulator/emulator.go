d8cf2408ecc01f581195166a1401c31975b2251d
Peter Siemens <peterjsiemens@gmail.com>
2020-04-21 07:33:53 -0700

    Allow emulator to read root key from config

--- flow/emulator/emulator.go
+++ flow/emulator/emulator.go
@@ -1,8 +1,14 @@
 package emulator
 
 import (
+	"fmt"
+
 	"github.com/dapperlabs/flow-emulator/cmd/emulator/start"
+	"github.com/onflow/flow-go-sdk/crypto"
 	"github.com/spf13/cobra"
+
+	cli "github.com/dapperlabs/flow-cli/flow"
+	"github.com/dapperlabs/flow-cli/flow/initialize"
 )
 
 var Cmd = &cobra.Command{
@@ -11,6 +17,22 @@ var Cmd = &cobra.Command{
 	TraverseChildren: true,
 }
 
+func configuredRootKey(init bool) (crypto.PrivateKey, crypto.SignatureAlgorithm, crypto.HashAlgorithm) {
+	var rootAcct *cli.Account
+
+	if init {
+		pconf := initialize.InitProject()
+		rootAcct = pconf.RootAccount()
+
+		fmt.Printf("⚙️   Flow client initialized with root account:\n\n")
+		fmt.Printf("👤  Address: 0x%s\n", rootAcct.Address)
+	}
+
+	rootAcct = cli.LoadConfig().RootAccount()
+
+	return rootAcct.PrivateKey, rootAcct.SigAlgo, rootAcct.HashAlgo
+}
+
 func init() {
-	Cmd.AddCommand(start.Cmd)
+	Cmd.AddCommand(start.Cmd(configuredRootKey))
 }
