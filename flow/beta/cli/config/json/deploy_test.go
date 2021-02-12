7f2953bbc49fc4fe6d0ca2271b038cea91e77640
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-02-12 15:44:47 +0100

    deploy to json

--- flow/beta/cli/config/json/deploy_test.go
+++ flow/beta/cli/config/json/deploy_test.go
@@ -22,6 +22,7 @@ func Test_ConfigDeploySimple(t *testing.T) {
 	json.Unmarshal(b, &jsonDeploys)
 	deploys := jsonDeploys.transformToConfig()
 
+	//TODO: fix test to be sorted since its not necessary correct order
 	assert.Equal(t, "account-2", deploys.GetByNetwork("testnet")[0].Account)
 	assert.Equal(t, []string{"FungibleToken", "NonFungibleToken", "Kibble", "KittyItems"}, deploys.GetByNetwork("testnet")[0].Contracts)
 
@@ -30,4 +31,17 @@ func Test_ConfigDeploySimple(t *testing.T) {
 	assert.Equal(t, "account-4", deploys.GetByNetwork("emulator")[1].Account)
 	assert.Equal(t, []string{"KittyItems", "KittyItemsMarket"}, deploys.GetByNetwork("emulator")[0].Contracts)
 	assert.Equal(t, []string{"FungibleToken", "NonFungibleToken", "Kibble", "KittyItems", "KittyItemsMarket"}, deploys.GetByNetwork("emulator")[1].Contracts)
+}
+
+func Test_TransformDeployToJSON(t *testing.T) {
+	b := []byte(`{"emulator":{"account-3":["KittyItems","KittyItemsMarket"],"account-4":["FungibleToken","NonFungibleToken","Kibble","KittyItems","KittyItemsMarket"]},"testnet":{"account-2":["FungibleToken","NonFungibleToken","Kibble","KittyItems"]}}`)
+
+	var jsonDeploys jsonDeploys
+	json.Unmarshal(b, &jsonDeploys)
+	deploys := jsonDeploys.transformToConfig()
+
+	j := jsonDeploys.transformToJSON(deploys)
+	x, _ := json.Marshal(j)
+
+	assert.Equal(t, string(b), string(x))
 }
