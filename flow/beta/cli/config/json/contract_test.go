fab94536b5315364eb78fd1700b6009d5489b096
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-02-12 16:01:46 +0100

    comments

--- flow/beta/cli/config/json/contract_test.go
+++ flow/beta/cli/config/json/contract_test.go
@@ -1,3 +1,20 @@
+/*
+ * Flow CLI
+ *
+ * Copyright 2019-2020 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package json
 
 import (
@@ -51,7 +68,7 @@ func Test_ConfigContractsComplex(t *testing.T) {
 	assert.Equal(t, "./cadence/kittyItemsMarket/contracts/KittyItemsMarket.cdc", contracts.GetByNetwork("emulator")[1].Source)
 }
 
-func Test_TransformToJSON(t *testing.T) {
+func Test_TransformContractToJSON(t *testing.T) {
 	b := []byte(`{"KittyItems":"./cadence/kittyItems/contracts/KittyItems.cdc","KittyItemsMarket":{"emulator":"./cadence/kittyItemsMarket/contracts/KittyItemsMarket.cdc","testnet":"0x123123123"}}`)
 
 	var jsonContracts jsonContracts
