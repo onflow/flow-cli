3d39f5cf7b3509c4cb32a50656574e54f9293900
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-02-12 14:34:50 +0100

    contract to json

--- flow/beta/cli/config/json/contract_test.go
+++ flow/beta/cli/config/json/contract_test.go
@@ -34,6 +34,8 @@ func Test_ConfigContractsComplex(t *testing.T) {
 	json.Unmarshal(b, &jsonContracts)
 	contracts := jsonContracts.transformToConfig()
 
+	assert.Equal(t, len(contracts), 3)
+
 	assert.Equal(t, "./cadence/kittyItems/contracts/KittyItems.cdc", contracts.GetByName("KittyItems").Source)
 	assert.Equal(t, "./cadence/kittyItemsMarket/contracts/KittyItemsMarket.cdc", contracts.GetByNameAndNetwork("KittyItemsMarket", "emulator").Source)
 	assert.Equal(t, "0x123123123", contracts.GetByNameAndNetwork("KittyItemsMarket", "testnet").Source)
@@ -41,9 +43,23 @@ func Test_ConfigContractsComplex(t *testing.T) {
 	assert.Equal(t, 2, len(contracts.GetByNetwork("testnet")))
 	assert.Equal(t, 2, len(contracts.GetByNetwork("emulator")))
 
+	//REF: sort result since it is not arranged
 	assert.Equal(t, "./cadence/kittyItems/contracts/KittyItems.cdc", contracts.GetByNetwork("testnet")[0].Source)
 	assert.Equal(t, "0x123123123", contracts.GetByNetwork("testnet")[1].Source)
 
 	assert.Equal(t, "./cadence/kittyItems/contracts/KittyItems.cdc", contracts.GetByNetwork("emulator")[0].Source)
 	assert.Equal(t, "./cadence/kittyItemsMarket/contracts/KittyItemsMarket.cdc", contracts.GetByNetwork("emulator")[1].Source)
+}
+
+func Test_TransformToJSON(t *testing.T) {
+	b := []byte(`{"KittyItems":"./cadence/kittyItems/contracts/KittyItems.cdc","KittyItemsMarket":{"emulator":"./cadence/kittyItemsMarket/contracts/KittyItemsMarket.cdc","testnet":"0x123123123"}}`)
+
+	var jsonContracts jsonContracts
+	json.Unmarshal(b, &jsonContracts)
+	contracts := jsonContracts.transformToConfig()
+
+	j := jsonContracts.transformToJSON(contracts)
+	x, _ := json.Marshal(j)
+
+	assert.Equal(t, string(b), string(x))
 }
