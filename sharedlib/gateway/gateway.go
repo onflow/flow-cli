84657918c7376872a74ccf1993ea3ed162eb56aa
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-03-08 16:13:15 +0100

    gateway added

---
+++ sharedlib/gateway/gateway.go
@@ -0,0 +1,12 @@
+package gateway
+
+import (
+	"github.com/onflow/flow-cli/flow/cli"
+	"github.com/onflow/flow-go-sdk"
+)
+
+type Gateway interface {
+	GetAccount(flow.Address) (*flow.Account, error)
+	SendTransaction(*flow.Transaction, *cli.Account) (*flow.Transaction, error)
+	GetTransactionResult(*flow.Transaction) (*flow.TransactionResult, error)
+}
