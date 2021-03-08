6c3d02ffef33180a2e63cd1a26ec7681c1d6eb05
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-03-08 16:12:23 +0100

    add emulator tbd

---
+++ sharedlib/gateway/emulator.go
@@ -0,0 +1,38 @@
+package gateway
+
+import (
+	"fmt"
+	"github.com/onflow/flow-cli/flow/cli"
+	emulator "github.com/onflow/flow-emulator"
+	"github.com/onflow/flow-go-sdk"
+)
+
+type EmulatorGateway struct {
+	emulator *emulator.Blockchain
+}
+
+func NewEmulatorGateway() *EmulatorGateway {
+	return &EmulatorGateway{
+		emulator: newEmulator(),
+	}
+}
+
+func newEmulator() *emulator.Blockchain {
+	b, err := emulator.NewBlockchain()
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func (g *EmulatorGateway) GetAccount(address flow.Address) (*flow.Account, error) {
+	return g.emulator.GetAccount(address)
+}
+
+func (g *EmulatorGateway) SendTransaction(tx *flow.Transaction, signer *cli.Account) (*flow.Transaction, error) {
+	return nil, fmt.Errorf("Not Supported Yet")
+}
+
+func (g *EmulatorGateway) GetTransactionResult(tx *flow.Transaction) (*flow.TransactionResult, error) {
+	return nil, fmt.Errorf("Not Supported Yet")
+}
