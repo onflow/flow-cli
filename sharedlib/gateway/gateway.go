f27b6c488471961342862a4d55e1e4c47e988d44
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-03-09 17:14:18 +0100

    added execute script

--- sharedlib/gateway/gateway.go
+++ sharedlib/gateway/gateway.go
@@ -1,6 +1,7 @@
 package gateway
 
 import (
+	"github.com/onflow/cadence"
 	"github.com/onflow/flow-cli/flow/cli"
 	"github.com/onflow/flow-go-sdk"
 )
@@ -9,4 +10,5 @@ type Gateway interface {
 	GetAccount(flow.Address) (*flow.Account, error)
 	SendTransaction(*flow.Transaction, *cli.Account) (*flow.Transaction, error)
 	GetTransactionResult(*flow.Transaction) (*flow.TransactionResult, error)
+	ExecuteScript([]byte, []cadence.Value) (cadence.Value, error)
 }
