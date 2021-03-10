cf69e1e86c71957ae266d06472d6e5c620ee10c5
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-03-10 16:00:12 +0100

    added collection service

---
+++ sharedlib/services/collections.go
@@ -0,0 +1,27 @@
+package services
+
+import (
+	"github.com/onflow/flow-cli/flow/cli"
+	"github.com/onflow/flow-cli/sharedlib/gateway"
+	"github.com/onflow/flow-go-sdk"
+)
+
+// Collections service handles all interactions for collections
+type Collections struct {
+	gateway gateway.Gateway
+	project cli.Project
+}
+
+// NewCollections create new collection service
+func NewCollections(gateway gateway.Gateway, project cli.Project) *Collections {
+	return &Collections{
+		gateway: gateway,
+		project: project,
+	}
+}
+
+// Get collection
+func (c *Collections) Get(id string) (*flow.Collection, error) {
+	collectionID := flow.HexToID(id)
+	return c.gateway.GetCollection(collectionID)
+}
