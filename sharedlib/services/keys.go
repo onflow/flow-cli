cc87c7f1ac1cfbada1f387fa4859ad41a5f3e15f
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-03-10 12:38:06 +0100

    added keys service

---
+++ sharedlib/services/keys.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/onflow/flow-cli/flow/cli"
+	"github.com/onflow/flow-cli/sharedlib/gateway"
+	"github.com/onflow/flow-go-sdk/crypto"
+)
+
+// Keys service handles all interactions for keys
+type Keys struct {
+	gateway gateway.Gateway
+	project cli.Project
+}
+
+// NewTransactions create new transaction service
+func NewKeys(gateway gateway.Gateway, project cli.Project) *Keys {
+	return &Keys{
+		gateway: gateway,
+		project: project,
+	}
+}
+
+func (k *Keys) Generate(inputSeed string, signatureAlgo string) (*crypto.PrivateKey, error) {
+	var seed []byte
+	if inputSeed == "" {
+		seed = cli.RandomSeed(crypto.MinSeedLength)
+	} else {
+		seed = []byte(inputSeed)
+	}
+
+	sigAlgo := crypto.StringToSignatureAlgorithm(signatureAlgo)
+	if sigAlgo == crypto.UnknownSignatureAlgorithm {
+		return nil, fmt.Errorf("invalid signature algorithm: %s", signatureAlgo)
+	}
+
+	privateKey, err := crypto.GeneratePrivateKey(sigAlgo, seed)
+	return &privateKey, err
+}
