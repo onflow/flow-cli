0ac1a40d35a3e61d2682756c6873b3b371e7fb5e
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-03-10 12:35:12 +0100

    added interface gateway methods

--- sharedlib/gateway/gateway.go
+++ sharedlib/gateway/gateway.go
@@ -4,11 +4,15 @@ import (
 	"github.com/onflow/cadence"
 	"github.com/onflow/flow-cli/flow/cli"
 	"github.com/onflow/flow-go-sdk"
+	"github.com/onflow/flow-go-sdk/client"
 )
 
 type Gateway interface {
 	GetAccount(flow.Address) (*flow.Account, error)
 	SendTransaction(*flow.Transaction, *cli.Account) (*flow.Transaction, error)
 	GetTransactionResult(*flow.Transaction) (*flow.TransactionResult, error)
+	GetTransaction(flow.Identifier) (*flow.Transaction, error)
 	ExecuteScript([]byte, []cadence.Value) (cadence.Value, error)
+	GetLatestBlock() (*flow.Block, error)
+	GetEvents(string, uint64, uint64) ([]client.BlockEvents, error)
 }
