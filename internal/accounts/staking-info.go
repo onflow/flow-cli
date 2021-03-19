2993e58121f891deb72b03da3921cae6ac492de7
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-03-19 15:09:27 +0100

    improved staking info

--- internal/accounts/staking-info.go
+++ internal/accounts/staking-info.go
@@ -58,8 +58,8 @@ type StakingResult struct {
 // JSON convert result to JSON
 func (r *StakingResult) JSON() interface{} {
 	result := make(map[string]interface{}, 0)
-	result["staking"] = *r.staking
-	result["delegation"] = *r.delegation
+	result["staking"] = flow.NewStakingInfoFromValue(*r.staking)
+	result["delegation"] = flow.NewStakingInfoFromValue(*r.delegation)
 
 	return result
 }
@@ -70,10 +70,33 @@ func (r *StakingResult) String() string {
 	writer := tabwriter.NewWriter(&b, 0, 8, 1, '\t', tabwriter.AlignRight)
 
 	fmt.Fprintf(writer, "Account Staking Info:\n")
-	fmt.Fprintf(writer, "%v\n\n", *r.staking)
 
-	fmt.Fprintf(writer, "Account Delegation Info:\n")
-	fmt.Fprintf(writer, "%v\n", *r.delegation)
+	stakingInfo := flow.NewStakingInfoFromValue(*r.staking)
+
+	fmt.Fprintf(writer, "ID: \t %v\n", stakingInfo["id"])
+	fmt.Fprintf(writer, "Initial Weight: \t %v\n", stakingInfo["initialWeight"])
+	fmt.Fprintf(writer, "Networking Address: \t %v\n", stakingInfo["networkingAddress"])
+	fmt.Fprintf(writer, "Networking Key: \t %v\n", stakingInfo["networkingKey"])
+	fmt.Fprintf(writer, "Role: \t %v\n", stakingInfo["role"])
+	fmt.Fprintf(writer, "Staking Key: \t %v\n", stakingInfo["stakingKey"])
+	fmt.Fprintf(writer, "Tokens Committed: \t %v\n", stakingInfo["tokensCommitted"])
+	fmt.Fprintf(writer, "Tokens To Unstake: \t %v\n", stakingInfo["tokensRequestedToUnstake"])
+	fmt.Fprintf(writer, "Tokens Rewarded: \t %v\n", stakingInfo["tokensRewarded"])
+	fmt.Fprintf(writer, "Tokens Staked: \t %v\n", stakingInfo["tokensStaked"])
+	fmt.Fprintf(writer, "Tokens Unstaked: \t %v\n", stakingInfo["tokensUnstaked"])
+	fmt.Fprintf(writer, "Tokens Unstaking: \t %v\n", stakingInfo["tokensUnstaking"])
+	fmt.Fprintf(writer, "Total Tokens Staked: \t %v\n", stakingInfo["totalTokensStaked"])
+
+	delegationStakingInfo := flow.NewStakingInfoFromValue(*r.delegation)
+
+	fmt.Fprintf(writer, "\n\nAccount Delegation Info:\n")
+	fmt.Fprintf(writer, "ID: \t %v\n", delegationStakingInfo["id"])
+	fmt.Fprintf(writer, "Tokens Committed: \t %v\n", delegationStakingInfo["tokensCommitted"])
+	fmt.Fprintf(writer, "Tokens To Unstake: \t %v\n", delegationStakingInfo["tokensRequestedToUnstake"])
+	fmt.Fprintf(writer, "Tokens Rewarded: \t %v\n", delegationStakingInfo["tokensRewarded"])
+	fmt.Fprintf(writer, "Tokens Staked: \t %v\n", delegationStakingInfo["tokensStaked"])
+	fmt.Fprintf(writer, "Tokens Unstaked: \t %v\n", delegationStakingInfo["tokensUnstaked"])
+	fmt.Fprintf(writer, "Tokens Unstaking: \t %v\n", delegationStakingInfo["tokensUnstaking"])
 
 	writer.Flush()
 	return b.String()
