a204dadcbcc1f0a80e06b3b069c8b26601008a41
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-03-30 20:06:38 +0200

    show error as part of result if present

--- internal/transactions/transactions.go
+++ internal/transactions/transactions.go
@@ -52,6 +52,7 @@ func (r *TransactionResult) JSON() interface{} {
 	result := make(map[string]string)
 	result["Hash"] = r.tx.ID().String()
 	result["Status"] = r.result.Status.String()
+	result["Error"] = r.result.Error.Error()
 
 	if r.result != nil {
 		result["Events"] = fmt.Sprintf("%s", r.result.Events)
@@ -69,10 +70,14 @@ func (r *TransactionResult) String() string {
 	fmt.Fprintf(writer, "Status\t %s\n", r.result.Status)
 	fmt.Fprintf(writer, "Payer\t %s\n", r.tx.Payer.Hex())
 
-	events := events.EventResult{
+	if r.result.Error != nil {
+		fmt.Fprintf(writer, "\n❌ Transaction Error \n%s\n", r.result.Error.Error())
+	}
+
+	e := events.EventResult{
 		Events: r.result.Events,
 	}
-	fmt.Fprintf(writer, "Events\t %s\n", events.String())
+	fmt.Fprintf(writer, "Events\t %s\n", e.String())
 
 	if r.code {
 		fmt.Fprintf(writer, "Code\n\n%s\n", r.tx.Script)
