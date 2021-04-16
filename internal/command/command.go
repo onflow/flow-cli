c3329f616396ac983fc24eda2c93d395bef8e7b9
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-04-16 15:21:50 +0200

    conflict resolution

--- internal/command/command.go
+++ internal/command/command.go
@@ -27,13 +27,13 @@ import (
 	"os"
 	"strings"
 
+	"github.com/onflow/flow-cli/build"
 	"github.com/onflow/flow-cli/pkg/flowcli/config"
 	"github.com/onflow/flow-cli/pkg/flowcli/gateway"
 	"github.com/onflow/flow-cli/pkg/flowcli/output"
 	"github.com/onflow/flow-cli/pkg/flowcli/project"
 	"github.com/onflow/flow-cli/pkg/flowcli/services"
 	"github.com/onflow/flow-cli/pkg/flowcli/util"
-	"github.com/onflow/flow-cli/build"
 
 	"github.com/onflow/flow-go-sdk/client"
 	"github.com/psiemens/sconfig"
