8c098f54f276210ba63a7b39c8fa8d8514bd3c27
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-04-19 14:32:44 +0200

    network default value in compare

--- internal/command/command.go
+++ internal/command/command.go
@@ -209,7 +209,7 @@ func createGateway(host string) (gateway.Gateway, error) {
 // resolveHost from the flags provided
 func resolveHost(proj *project.Project, hostFlag string, networkFlag string) (string, error) {
 	// don't allow both network and host flag as the host might be different
-	if networkFlag != "" && hostFlag != "" {
+	if networkFlag != config.DefaultEmulatorNetwork().Name && hostFlag != "" {
 		return "", fmt.Errorf("shouldn't use both host and network flags, better to use network flag")
 	}
 
