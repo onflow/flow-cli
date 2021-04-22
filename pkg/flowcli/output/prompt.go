32a2f9821754a6a32e88d4787d1bd1f0cefda9b2
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-04-22 21:00:51 +0200

    add prompts for adding config

--- pkg/flowcli/output/prompt.go
+++ pkg/flowcli/output/prompt.go
@@ -20,6 +20,11 @@ package output
 
 import (
 	"fmt"
+	"net/url"
+
+	"github.com/onflow/flow-cli/pkg/flowcli/util"
+
+	"github.com/onflow/flow-cli/pkg/flowcli/config"
 
 	"github.com/gosuri/uilive"
 	"github.com/manifoldco/promptui"
@@ -90,4 +95,204 @@ func ApproveTransactionPrompt(transaction *project.Transaction) bool {
 	writer.Flush()
 
 	return result == "Yes"
+}
+
+func namePrompt() string {
+	namePrompt := promptui.Prompt{
+		Label: "Name",
+		Validate: func(s string) error {
+			if len(s) < 1 {
+				return fmt.Errorf("invalid name")
+			}
+			return nil
+		},
+	}
+
+	name, _ := namePrompt.Run()
+	return name
+}
+
+func addressPrompt() string {
+	addressPrompt := promptui.Prompt{
+		Label: "Address",
+		Validate: func(s string) error {
+			_, err := config.StringToAddress(s)
+			return err
+		},
+	}
+
+	address, _ := addressPrompt.Run()
+	return address
+}
+
+func contractAliasPrompt(contractName string) (string, string) {
+	networks := []string{"emulator", "testnet"}
+
+	aliasesPrompt := promptui.Select{
+		Label: fmt.Sprintf(
+			"Does contract [%s] have any additional aliases: \nYou can read about aliases here: https://docs.onflow.org/flow-cli/project-contracts/", // todo check
+			contractName,
+		),
+		Items: []string{"No", "Yes, Emulator Alias", "Yes, Testnet Alias"},
+	}
+	i, answer, _ := aliasesPrompt.Run()
+	return answer, networks[i]
+}
+
+func contractPrompt(contractNames []string) string {
+	contractPrompt := promptui.Select{
+		Label: "Choose contract you wish to deploy",
+		Items: contractNames,
+	}
+	_, contractName, _ := contractPrompt.Run()
+
+	return contractName
+}
+
+func addAnotherContractToDeploymentPrompt() bool {
+	addContractPrompt := promptui.Select{
+		Label: "Do you wish to add another contract for deployment?",
+		Items: []string{"No", "Yes"},
+	}
+	_, addMore, _ := addContractPrompt.Run()
+	return addMore == "Yes"
+}
+
+func NewAccountPrompt() map[string]string {
+	accountData := make(map[string]string)
+
+	accountData["name"] = namePrompt()
+	accountData["address"] = addressPrompt()
+
+	sigAlgoPrompt := promptui.Select{
+		Label: "Signature algorithm",
+		Items: []string{"ECDSA_P256", "ECDSA_secp256k1"},
+	}
+	_, accountData["sigAlgo"], _ = sigAlgoPrompt.Run()
+
+	hashAlgoPrompt := promptui.Select{
+		Label: "Hashing algorithm",
+		Items: []string{"SHA3_256", "SHA2_256"},
+	}
+	_, accountData["hashAlgo"], _ = hashAlgoPrompt.Run()
+
+	keyPrompt := promptui.Prompt{
+		Label: "Private key",
+		Validate: func(s string) error {
+			_, err := config.StringToHexKey(s, accountData["sigAlgo"])
+			return err
+		},
+	}
+	accountData["key"], _ = keyPrompt.Run()
+
+	keyIndexPrompt := promptui.Prompt{
+		Label:   "Key index (Default: 0)",
+		Default: "0",
+		Validate: func(s string) error {
+			_, err := config.StringToKeyIndex(s)
+			return err
+		},
+	}
+	accountData["keyIndex"], _ = keyIndexPrompt.Run()
+
+	return accountData
+}
+
+func NewContractPrompt() map[string]string {
+	contractData := make(map[string]string)
+
+	contractData["name"] = namePrompt()
+
+	sourcePrompt := promptui.Prompt{
+		Label: "Contract file location",
+		Validate: func(s string) error {
+			if !config.Exists(s) {
+				return fmt.Errorf("contract file doesn't exist: %s", s)
+			}
+
+			return nil
+		},
+	}
+	contractData["source"], _ = sourcePrompt.Run()
+
+	aliasAnswer, network := contractAliasPrompt(contractData["name"])
+	for aliasAnswer != "No" {
+		aliasAddress := addressPrompt()
+		contractData[network] = aliasAddress
+
+		aliasAnswer, network = contractAliasPrompt(contractData["name"])
+	}
+
+	return contractData
+}
+
+func NewNetworkPrompt() map[string]string {
+	networkData := make(map[string]string)
+	networkData["name"] = namePrompt()
+
+	hostPrompt := promptui.Prompt{
+		Label: "Enter host location",
+		Validate: func(s string) error {
+			_, err := url.ParseRequestURI(s)
+			return err
+		},
+	}
+	networkData["host"], _ = hostPrompt.Run()
+
+	return networkData
+}
+
+func NewDeploymentPrompt(conf *config.Config) map[string]interface{} {
+	deploymentData := make(map[string]interface{})
+
+	networkNames := make([]string, 0)
+	for _, network := range conf.Networks {
+		networkNames = append(networkNames, network.Name)
+	}
+
+	networkPrompt := promptui.Select{
+		Label: "Choose network for deployment",
+		Items: networkNames,
+	}
+	_, deploymentData["network"], _ = networkPrompt.Run()
+
+	accountNames := make([]string, 0)
+	for _, account := range conf.Accounts {
+		accountNames = append(accountNames, account.Name)
+	}
+
+	accountPrompt := promptui.Select{
+		Label: "Choose an account to deploy to",
+		Items: accountNames,
+	}
+	_, deploymentData["account"], _ = accountPrompt.Run()
+
+	contractNames := make([]string, 0)
+	for _, contract := range conf.Contracts {
+		contractNames = append(contractNames, contract.Name)
+	}
+
+	deploymentData["contracts"] = make([]string, 0)
+
+	contractName := contractPrompt(contractNames)
+	deploymentData["contracts"] = append(deploymentData["contracts"].([]string), contractName)
+	contractNames = util.RemoveFromStringArray(contractNames, contractName)
+
+	for addAnotherContractToDeploymentPrompt() {
+		contractName := contractPrompt(contractNames)
+		deploymentData["contracts"] = append(deploymentData["contracts"].([]string), contractName)
+		contractNames = util.RemoveFromStringArray(contractNames, contractName)
+
+		if len(contractNames) == 0 {
+			break
+		}
+	}
+
+	return deploymentData
+}
+
+func RemoveAccountPrompt(conf *config.Config) {
+
+	// list and remove
+
 }
