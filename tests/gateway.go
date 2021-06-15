63a7983214b05ed11541639f0b171d70316571a9
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-06-15 12:50:31 +0200

    testify mock impl

--- tests/gateway.go
+++ tests/gateway.go
@@ -19,155 +19,75 @@
 package tests
 
 import (
-	"fmt"
-	"reflect"
-	"runtime"
-	"testing"
-
-	"github.com/onflow/cadence"
-	"github.com/onflow/flow-cli/pkg/flowkit"
+	"github.com/onflow/flow-cli/tests/mocks"
 	"github.com/onflow/flow-go-sdk"
 	"github.com/onflow/flow-go-sdk/client"
+	"github.com/stretchr/testify/mock"
+)
+
+const (
+	GetAccountFunc            = "GetAccount"
+	SendSignedTransactionFunc = "SendSignedTransaction"
+	GetCollectionFunc         = "GetCollection"
+	GetTransactionResultFunc  = "GetTransactionResult"
+	GetEventsFunc             = "GetEvents"
+	GetLatestBlockFunc        = "GetLatestBlock"
 )
 
 type TestGateway struct {
-	GetAccountMock                func(address flow.Address) (*flow.Account, error)
-	SendTransactionMock           func(tx *flowkit.Transaction) (*flow.Transaction, error)
-	PrepareTransactionPayloadMock func(tx *flowkit.Transaction) (*flowkit.Transaction, error)
-	SendSignedTransactionMock     func(tx *flowkit.Transaction) (*flow.Transaction, error)
-	GetTransactionResultMock      func(tx *flow.Transaction) (*flow.TransactionResult, error)
-	GetTransactionMock            func(id flow.Identifier) (*flow.Transaction, error)
-	ExecuteScriptMock             func(script []byte, arguments []cadence.Value) (cadence.Value, error)
-	GetLatestBlockMock            func() (*flow.Block, error)
-	GetEventsMock                 func(name string, start uint64, end uint64) ([]client.BlockEvents, error)
-	GetCollectionMock             func(id flow.Identifier) (*flow.Collection, error)
-	GetBlockByHeightMock          func(height uint64) (*flow.Block, error)
-	GetBlockByIDMock              func(id flow.Identifier) (*flow.Block, error)
-	PingMock                      func() error
-	functionsCalled               []interface{}
+	Mock                  *mocks.Gateway
+	SendSignedTransaction *mock.Call
+	GetAccount            *mock.Call
+	GetCollection         *mock.Call
+	GetTransactionResult  *mock.Call
+	GetEvents             *mock.Call
+	GetLatestBlock        *mock.Call
 }
 
 func DefaultMockGateway() *TestGateway {
-	return &TestGateway{
-		SendSignedTransactionMock: func(tx *flowkit.Transaction) (*flow.Transaction, error) {
-			return tx.FlowTransaction(), nil
-		},
-		GetLatestBlockMock: func() (*flow.Block, error) {
-			return NewBlock(), nil
-		},
-		GetAccountMock: func(address flow.Address) (*flow.Account, error) {
-			return NewAccountWithAddress(address.String()), nil
-		},
-		GetTransactionResultMock: func(tx *flow.Transaction) (*flow.TransactionResult, error) {
-			return NewTransactionResult(nil), nil
-		},
-		GetEventsMock: func(name string, start uint64, end uint64) ([]client.BlockEvents, error) {
-			return []client.BlockEvents{}, nil
-		},
-	}
-}
-
-func (g *TestGateway) AssertFuncsCalled(t *testing.T, reset bool, funcs ...interface{}) {
-	if len(funcs) > len(g.functionsCalled) {
-		t.Error("not all required functions were called")
-	}
-
-	called, err := g.funcsCalled(funcs)
-	if reset {
-		g.functionsCalled = nil
-	}
-
-	if !called {
-		t.Error(err)
-	}
-}
-
-func (g *TestGateway) AssertFuncsNotCalled(t *testing.T, reset bool, funcs ...interface{}) {
-	called, err := g.funcsCalled(funcs)
-	if reset {
-		g.functionsCalled = nil
+	m := &mocks.Gateway{}
+	t := &TestGateway{
+		Mock: m,
+		SendSignedTransaction: m.On(
+			SendSignedTransactionFunc,
+			mock.AnythingOfType("*flowkit.Transaction"),
+		),
+		GetAccount: m.On(
+			GetAccountFunc,
+			mock.AnythingOfType("flow.Address"),
+		),
+		GetCollection: m.On(
+			GetCollectionFunc,
+			mock.AnythingOfType("flow.Identifier"),
+		),
+		GetTransactionResult: m.On(
+			GetTransactionResultFunc,
+			mock.AnythingOfType("*flow.Transaction"),
+			mock.AnythingOfType("bool"),
+		),
+		GetEvents: m.On(
+			GetEventsFunc,
+			mock.AnythingOfType("string"),
+			mock.AnythingOfType("uint64"),
+			mock.AnythingOfType("uint64"),
+		),
+		GetLatestBlock: m.On(GetLatestBlockFunc),
 	}
 
-	if called {
-		t.Error(err)
-	}
-}
+	// default return values
+	t.SendSignedTransaction.Run(func(args mock.Arguments) {
+		t.SendSignedTransaction.Return(NewTransaction(), nil)
+	})
 
-func (g *TestGateway) funcCall(f interface{}) {
-	g.functionsCalled = append(g.functionsCalled, f)
-}
+	t.GetAccount.Run(func(args mock.Arguments) {
+		addr := args.Get(0).(flow.Address)
+		t.GetAccount.Return(NewAccountWithAddress(addr.String()), nil)
+	})
 
-func (g *TestGateway) funcsCalled(funcs []interface{}) (bool, error) {
-	for _, f := range funcs {
-		for x, c := range g.functionsCalled {
-			fp := reflect.ValueOf(f).Pointer()
-			cp := reflect.ValueOf(c).Pointer()
-			// if function called is same as required func called
-			if fp == cp {
-				break
-			} else if x == len(g.functionsCalled)-1 { // if last iteration without a match
-				return false, fmt.Errorf(
-					"required function %s not called",
-					runtime.FuncForPC(fp).Name(),
-				)
-			}
-		}
-	}
+	t.GetCollection.Return(NewCollection(), nil)
+	t.GetTransactionResult.Return(NewTransactionResult(nil), nil)
+	t.GetEvents.Return([]client.BlockEvents{}, nil)
+	t.GetLatestBlock.Return(NewBlock(), nil)
 
-	return true, nil
-}
-
-func (g *TestGateway) GetAccount(address flow.Address) (*flow.Account, error) {
-	g.funcCall(g.GetAccount)
-	return g.GetAccountMock(address)
-}
-
-func (g *TestGateway) SendSignedTransaction(tx *flowkit.Transaction) (*flow.Transaction, error) {
-	g.funcCall(g.SendSignedTransaction)
-	return g.SendSignedTransactionMock(tx)
-}
-
-func (g *TestGateway) GetTransactionResult(tx *flow.Transaction, waitSeal bool) (*flow.TransactionResult, error) {
-	g.funcCall(g.GetTransactionResult)
-	return g.GetTransactionResultMock(tx)
-}
-
-func (g *TestGateway) GetTransaction(id flow.Identifier) (*flow.Transaction, error) {
-	g.funcCall(g.GetTransaction)
-	return g.GetTransactionMock(id)
-}
-
-func (g *TestGateway) ExecuteScript(script []byte, arguments []cadence.Value) (cadence.Value, error) {
-	g.funcCall(g.ExecuteScript)
-	return g.ExecuteScriptMock(script, arguments)
-}
-
-func (g *TestGateway) GetLatestBlock() (*flow.Block, error) {
-	g.funcCall(g.GetLatestBlock)
-	return g.GetLatestBlockMock()
-}
-
-func (g *TestGateway) GetBlockByID(id flow.Identifier) (*flow.Block, error) {
-	g.funcCall(g.GetBlockByID)
-	return g.GetBlockByIDMock(id)
-}
-
-func (g *TestGateway) GetBlockByHeight(height uint64) (*flow.Block, error) {
-	g.funcCall(g.GetBlockByHeight)
-	return g.GetBlockByHeightMock(height)
-}
-
-func (g *TestGateway) GetEvents(name string, start uint64, end uint64) ([]client.BlockEvents, error) {
-	g.funcCall(g.GetEvents)
-	return g.GetEventsMock(name, start, end)
-}
-
-func (g *TestGateway) GetCollection(id flow.Identifier) (*flow.Collection, error) {
-	g.funcCall(g.GetCollection)
-	return g.GetCollectionMock(id)
-}
-
-func (g *TestGateway) Ping() error {
-	g.funcCall(g.Ping)
-	return nil
+	return t
 }
