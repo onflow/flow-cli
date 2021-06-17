5401c05fee340dff3794ee4790b0731104885db5
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-06-17 13:00:43 +0200

    keys tests

--- pkg/flowkit/services/keys_test.go
+++ pkg/flowkit/services/keys_test.go
@@ -63,13 +63,19 @@ func TestKeys(t *testing.T) {
 		}
 	})
 
-	t.Run("Decode RLP key", func(t *testing.T) {
+	t.Run("Decode RLP Key", func(t *testing.T) {
 		_, s, _ := setup()
 		dkey, err := s.Keys.DecodeRLP("f847b84084d716c14b051ad6b001624f738f5d302636e6b07cc75e4530af7776a4368a2b586dbefc0564ee28384c2696f178cbed52e62811bcc9ecb59568c996d342db2402038203e8")
 
 		assert.NoError(t, err)
 		assert.Equal(t, dkey.PublicKey.String(), "0x84d716c14b051ad6b001624f738f5d302636e6b07cc75e4530af7776a4368a2b586dbefc0564ee28384c2696f178cbed52e62811bcc9ecb59568c996d342db24")
 		assert.Equal(t, dkey.SigAlgo.String(), "ECDSA_P256")
+	})
+
+	t.Run("Decode RLP Key Invalid", func(t *testing.T) {
+		_, s, _ := setup()
+		_, err := s.Keys.DecodeRLP("aaa")
+		assert.Equal(t, err.Error(), "failed to decode public key: encoding/hex: odd length hex string")
 	})
 
 	t.Run("Decode PEM Key", func(t *testing.T) {
@@ -79,5 +85,11 @@ func TestKeys(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, dkey.PublicKey.String(), "0xd479b3cdc9edbddb195cb12b35161ade826b032a64bdd4062cc87fb3ba7e71c9cf646ff23990bb4532ca45c445c7e908cef278b2c4615360039a6660a366a95f")
 		assert.Equal(t, dkey.SigAlgo.String(), "ECDSA_P256")
+	})
+
+	t.Run("Decode PEM Key Invalid", func(t *testing.T) {
+		_, s, _ := setup()
+		_, err := s.Keys.DecodePEM("nope", crypto.ECDSA_P256)
+		assert.Equal(t, err.Error(), "crypto: failed to parse PEM string, not all bytes in PEM key were decoded: 6e6f7065")
 	})
 }
