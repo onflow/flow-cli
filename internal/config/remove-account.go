dc953d49165ddf3cf1791f545f91ca8be80bef50
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-08-24 16:25:11 +0200

    linted imports

--- internal/config/remove-account.go
+++ internal/config/remove-account.go
@@ -19,10 +19,10 @@
 package config
 
 import (
-	"github.com/onflow/flow-cli/pkg/flowkit"
 	"github.com/spf13/cobra"
 
 	"github.com/onflow/flow-cli/internal/command"
+	"github.com/onflow/flow-cli/pkg/flowkit"
 	"github.com/onflow/flow-cli/pkg/flowkit/output"
 	"github.com/onflow/flow-cli/pkg/flowkit/services"
 )
