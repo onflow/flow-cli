2c48ef73ca51f8afc68542289723dd7fec026f0d
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-09-21 16:07:42 +0200

    add verify command to top and refactor result

--- internal/signatures/signatures.go
+++ internal/signatures/signatures.go
@@ -1,8 +1,6 @@
 package signatures
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -14,21 +12,5 @@ var Cmd = &cobra.Command{
 
 func init() {
 	SignCommand.AddToParent(Cmd)
-}
-
-type SignatureResult struct {
-	result string
-}
-
-func (s *SignatureResult) JSON() interface{} {
-	return map[string]string{
-		"result": fmt.Sprintf("%x", s.result),
-	}
-}
-func (s *SignatureResult) String() string {
-	return fmt.Sprintf("%x", s.result)
-}
-
-func (s *SignatureResult) Oneliner() string {
-	return fmt.Sprintf("%x", s.result)
+	VerifyCommand.AddToParent(Cmd)
 }
