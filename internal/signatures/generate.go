ddfc6e0d3f80ce449735ba7b705ce24a184f2d3a
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-09-21 16:36:32 +0200

    improve generate output

--- internal/signatures/generate.go
+++ internal/signatures/generate.go
@@ -1,8 +1,11 @@
 package signatures
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+
+	"github.com/onflow/flow-cli/pkg/flowkit/util"
 
 	"github.com/onflow/flow-cli/internal/command"
 	"github.com/onflow/flow-cli/pkg/flowkit"
@@ -52,23 +55,54 @@ func sign(
 	}
 
 	return &SignatureResult{
-		result: string(signed),
+		result:  string(signed),
+		payload: string(payload),
+		key:     acc.Key(),
 	}, nil
 }
 
 type SignatureResult struct {
-	result string
+	result  string
+	payload string
+	key     flowkit.AccountKey
+}
+
+func (s *SignatureResult) pubKey() string {
+	pkey, err := s.key.PrivateKey()
+	if err == nil {
+		return (*pkey).PublicKey().String()
+	}
+
+	return "ERR"
 }
 
 func (s *SignatureResult) JSON() interface{} {
 	return map[string]string{
-		"result": fmt.Sprintf("%x", s.result),
+		"signature": fmt.Sprintf("%x", s.result),
+		"payload":   fmt.Sprintf("%s", s.payload),
+		"hashAlgo":  fmt.Sprintf("%s", s.key.HashAlgo()),
+		"sigAlgo":   fmt.Sprintf("%s", s.key.SigAlgo()),
+		"pubKey":    fmt.Sprintf("%s", s.pubKey()),
 	}
 }
 func (s *SignatureResult) String() string {
-	return fmt.Sprintf("signature: \t%x\n", s.result)
+	var b bytes.Buffer
+	writer := util.CreateTabWriter(&b)
+
+	_, _ = fmt.Fprintf(writer, "Signature \t %x\n", s.result)
+	_, _ = fmt.Fprintf(writer, "Payload \t %s\n", s.payload)
+	_, _ = fmt.Fprintf(writer, "Public Key \t %s\n", s.pubKey())
+	_, _ = fmt.Fprintf(writer, "Hash Algorithm \t %s\n", s.key.HashAlgo())
+	_, _ = fmt.Fprintf(writer, "Signature Algorithm \t %s\n", s.key.SigAlgo())
+
+	_ = writer.Flush()
+	return b.String()
 }
 
 func (s *SignatureResult) Oneliner() string {
-	return fmt.Sprintf("signature: %x", s.result)
+
+	return fmt.Sprintf(
+		"signature: %x, payload: %s, hashAlgo: %s, sigAlgo: %s, pubKey: %s",
+		s.result, s.payload, s.key.HashAlgo(), s.key.SigAlgo(), s.pubKey(),
+	)
 }
