7761b7b43a0bcc65a15f1736d637117e2a6c9bf8
sukantoraymond <rsukanto@umich.edu>
2022-07-01 12:13:10 -0400

    Update internal/command/command.go
    Co-authored-by: Peter Siemens <peterjsiemens@gmail.com>

--- internal/command/command.go
+++ internal/command/command.go
@@ -282,7 +282,7 @@ func initCrashReporting() {
 			if output.ReportCrash() {
 				return event
 			} else {
-				fmt.Printf("\nPlease help us improve the Flow CLI by opening an issue on https://github.com/onflow/flow-cli/issues, \nand pasting the output as well as description of the actions you took to cause this crash.\n\n")
+				fmt.Printf("\nPlease help us improve the Flow CLI by opening an issue on https://github.com/onflow/flow-cli/issues, \nand pasting the output as well as description of the actions you took that resulted in this crash.\n\n")
 				fmt.Println(hint.RecoveredException)
 				fmt.Println(event.Threads, event.Fingerprint)
 				fmt.Println(event.Contexts)
