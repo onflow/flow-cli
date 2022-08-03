d5f814911b563abc4e0c3f52b7b29402e624146d
sukantoraymond <rsukanto@umich.edu>
2022-08-03 16:35:20 -0400

    fix lint

--- internal/command/command.go
+++ internal/command/command.go
@@ -81,16 +81,15 @@ const (
 func (c Command) handleUserTracking() {
 	if util.MIXPANEL_SERVICE_ACCOUNT_SECRET == "" || util.MIXPANEL_PROJECT_TOKEN == "" {
 		return
-	} else {
-		optedIn, err := util.IsUserOptedIn()
+	}
+	optedIn, err := util.IsUserOptedIn()
+	if err != nil {
+		sentry.CaptureException(err)
+	}
+	if optedIn {
+		err = util.TrackCommandUsage(c.Cmd)
 		if err != nil {
 			sentry.CaptureException(err)
-		}
-		if optedIn {
-			err = util.TrackCommandUsage(c.Cmd)
-			if err != nil {
-				sentry.CaptureException(err)
-			}
 		}
 	}
 }
