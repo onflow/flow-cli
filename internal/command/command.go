431fb08524643c5e0fc0ff2adb40db6f697c6126
sukantoraymond <rsukanto@umich.edu>
2022-08-12 13:28:40 -0400

    fix lint

--- internal/command/command.go
+++ internal/command/command.go
@@ -82,7 +82,7 @@ func (c Command) handleUserTracking() {
 	if util.MIXPANEL_SERVICE_ACCOUNT_SECRET == "" || util.MIXPANEL_PROJECT_TOKEN == "" {
 		return
 	}
-	util.TrackCommandUsage(c.Cmd)
+	_ = util.TrackCommandUsage(c.Cmd)
 }
 
 // AddToParent add new command to main parent cmd
