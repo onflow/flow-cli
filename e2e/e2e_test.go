c6ed5beed7362fef4ecc71e5fbbb7727f248fe07
Neilzon Viloria <neilcviloria@gmail.com>
2022-08-26 10:31:28 -0600

    add tests for more commands

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -57,20 +57,15 @@ func Test_cliCommands(t *testing.T) {
 		elapsed = cur.Sub(start)
 	}
 
-	// ### KEYS ###
-	result = icmd.RunCmd(icmd.Command(FLOW_BINARY, "keys", "generate"))
-	result.Assert(t, icmd.Success)
-
 	// // ### ACCOUNTS AND SCRIPTS ###
 	cmd := icmd.Command(FLOW_BINARY, "accounts", "create")
 
 	buf := bytes.Buffer{}
 
-	accountName := "remy"
-	acctInput := accountName + "\n"
-	e2e.RespondToPrompt([]byte(acctInput), &buf)
-	e2e.RespondToPrompt([]byte{10}, &buf)      // second input: enter key
-	e2e.RespondToPrompt([]byte{121, 10}, &buf) // "y" for yes and enter key
+	accountName := "remy\n"
+	e2e.RespondToPrompt([]byte(accountName), &buf) // first prompt: account name
+	e2e.RespondToPrompt([]byte{10}, &buf)          // second prompt: enter key
+	e2e.RespondToPrompt([]byte{121, 10}, &buf)     // third prompt: "y" for yes and enter key
 	cmd.Stdin = &buf
 
 	result = icmd.RunCmd(cmd)
@@ -93,6 +88,9 @@ func Test_cliCommands(t *testing.T) {
 	assert.Equal(t, result.Stdout(), expected, "Outputs of updated contract should be the same")
 
 	// ### TRANSACTIONS ### //
+	result = icmd.RunCmd(icmd.Command(FLOW_BINARY, "transactions", "send", "./files/tx.cdc"))
+	result.Assert(t, icmd.Success)
+
 	cmd = icmd.Command(FLOW_BINARY, "transactions", "build", "./files/tx.cdc", "--filter", "payload", "--save", "./built.rlp")
 	defer icmd.RunCommand("rm", "built.rlp")
 	txBuf := bytes.Buffer{}
@@ -122,6 +120,46 @@ func Test_cliCommands(t *testing.T) {
 	result = icmd.RunCmd(icmd.Command(FLOW_BINARY, "transactions", "get", txId))
 	result.Assert(t, icmd.Success)
 
+	result = icmd.RunCmd(icmd.Command(FLOW_BINARY, "transactions", "decode", "./signed.rlp"))
+	result.Assert(t, icmd.Success)
+
 	result = icmd.RunCommand(FLOW_BINARY, "accounts", "remove-contract", "HelloWorld")
+	result.Assert(t, icmd.Success)
+
+	// ### BLOCKS ###
+	result = icmd.RunCommand(FLOW_BINARY, "blocks", "get", "latest")
+	result.Assert(t, icmd.Success)
+	re = regexp.MustCompile("\nBlock ID\t\t(.*?)\n")
+	matches = re.FindStringSubmatch(result.Stdout())
+	blockId := matches[1]
+
+	result = icmd.RunCommand(FLOW_BINARY, "blocks", "get", blockId)
+	result.Assert(t, icmd.Success)
+
+	result = icmd.RunCommand(FLOW_BINARY, "blocks", "get", "2")
+	result.Assert(t, icmd.Success)
+
+	// ### NETWORK ###
+	result = icmd.RunCommand(FLOW_BINARY, "status")
+	result.Assert(t, icmd.Success)
+
+	// ### KEYS ###
+	result = icmd.RunCmd(icmd.Command(FLOW_BINARY, "keys", "generate"))
+	result.Assert(t, icmd.Success)
+	re = regexp.MustCompile("\nPrivate Key \t (.*?) \n")
+	matches = re.FindStringSubmatch(result.Stdout())
+	privateKey := matches[1]
+
+	// ### CONFIG ###
+	cmd = icmd.Command(FLOW_BINARY, "config", "add", "account")
+	confBuf := bytes.Buffer{}
+	e2e.RespondToPrompt([]byte("joe\n"), &confBuf)
+	e2e.RespondToPrompt([]byte("01cf0e2f2f715450\n"), &confBuf)
+	e2e.RespondToPrompt([]byte{10}, &confBuf)
+	e2e.RespondToPrompt([]byte{10}, &confBuf)
+	e2e.RespondToPrompt([]byte(privateKey+"\n"), &confBuf)
+	e2e.RespondToPrompt([]byte("0\n"), &confBuf)
+	cmd.Stdin = &confBuf
+	result = icmd.RunCmd(cmd)
 	result.Assert(t, icmd.Success)
 }
