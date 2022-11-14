443cd17c92af1e221ec882f40df98d80a4a66a28
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-11-14 18:42:45 +0100

    fix temp placeholder

--- internal/tools/flowser.go
+++ internal/tools/flowser.go
@@ -24,6 +24,7 @@ import (
 	"github.com/onflow/flow-cli/internal/command"
 	"github.com/onflow/flow-cli/internal/settings"
 	"github.com/onflow/flow-cli/pkg/flowkit"
+	"github.com/onflow/flow-cli/pkg/flowkit/config"
 	"github.com/onflow/flow-cli/pkg/flowkit/output"
 	"github.com/onflow/flow-cli/pkg/flowkit/services"
 	"github.com/onflowser/flowser/pkg/flowser"
@@ -82,7 +83,7 @@ func runFlowser(
 	}
 
 	// check if current directory is existing flow project if not then don't pass project path to Flowser, so user can choose a project
-	_, err = state.ReadFile("foo")
+	_, err = state.ReadFile(config.DefaultPath)
 	if os.IsNotExist(err) {
 		projectPath = ""
 	}
