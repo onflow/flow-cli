5c3d6555000e5515d9aedcd07abd5d0cf03aecf5
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-12-13 14:14:13 +0100

    refactor for deployments

--- internal/super/dev.go
+++ internal/super/dev.go
@@ -19,15 +19,22 @@
 package super
 
 import (
-	"fmt"
 	"github.com/onflow/flow-cli/internal/command"
-	"github.com/onflow/flow-cli/internal/config"
 	"github.com/onflow/flow-cli/pkg/flowkit"
+	"github.com/onflow/flow-cli/pkg/flowkit/config"
+	"github.com/onflow/flow-cli/pkg/flowkit/output"
+	"github.com/onflow/flow-cli/pkg/flowkit/project"
 	"github.com/onflow/flow-cli/pkg/flowkit/services"
-	"github.com/spf13/cobra"
+	"github.com/onflow/flow-go-sdk"
+	"github.com/onflow/flow-go-sdk/crypto"
 	"os"
-	"path/filepath"
+
+	"github.com/spf13/cobra"
 )
+
+type flagsDev struct{}
+
+var devFlags = flagsDev{}
 
 var DevCommand = &command.Command{
 	Cmd: &cobra.Command{
@@ -36,7 +43,7 @@ var DevCommand = &command.Command{
 		Args:    cobra.ExactArgs(0),
 		Example: "flow dev",
 	},
-	Flags: &config.InitFlag,
+	Flags: &devFlags,
 	RunS:  dev,
 }
 
@@ -45,23 +52,125 @@ func dev(
 	readerWriter flowkit.ReaderWriter,
 	globalFlags command.GlobalFlags,
 	services *services.Services,
-	state *flowkit.State,
+	s *flowkit.State,
 ) (command.Result, error) {
 	dir, err := os.Getwd()
 	if err != nil {
 		return nil, err
 	}
 
-	proj := projectFiles{projectDir: filepath.Join(dir, "cadence")}
+	// todo dev work if not run on top root directory - at least have a warning
+	// todo handle emulator running as part of this service or part of existing running emulator
 
-	c, _ := proj.Contracts()
-	fmt.Println("contracts", c)
+	service, err := s.EmulatorServiceAccount()
+	if err != nil {
+		return nil, err
+	}
 
-	a, _ := proj.Accounts()
-	fmt.Println("accounts", a)
+	logger := output.NewStdoutLogger(output.NoneLog)
+	services.SetLogger(logger)
 
-	s, _ := proj.Scripts()
-	fmt.Println("scripts", s)
+	// todo maybe not optimal, test how it performs otherwise we will just keep the state and update changes, especially the account section is problematic, creating accounts everytime might be time consuming
+	// create new state everytime, just keep the service key the same
+	state, err := flowkit.Init(readerWriter, crypto.ECDSA_P256, crypto.SHA3_256)
+	if err != nil {
+		return nil, err
+	}
+
+	serviceKey, err := service.Key().PrivateKey()
+	if err != nil {
+		return nil, err
+	}
+	state.SetEmulatorKey(*serviceKey)
+
+	proj := newProjectFiles(dir)
+
+	deployments, err := proj.Deployments()
+	if err != nil {
+		return nil, err
+	}
+
+	for accName, contracts := range deployments {
+		if accName == "" {
+			accName = config.DefaultEmulatorServiceAccountName
+		}
+
+		err := addAccount(accName, service, services, state)
+		if err != nil {
+			return nil, err
+		}
+
+		contractDeployments := make([]config.ContractDeployment, len(contracts))
+		for i, path := range contracts {
+			contract, err := addContract(path, state, readerWriter)
+			if err != nil {
+				return nil, err
+			}
+			contractDeployments[i] = config.ContractDeployment{Name: contract.Name}
+		}
+
+		state.Deployments().AddOrUpdate(config.Deployment{
+			Network:   config.DefaultEmulatorNetwork().Name, // todo take in from flag
+			Account:   accName,
+			Contracts: contractDeployments,
+		})
+	}
+
+	err = state.SaveDefault()
+	if err != nil {
+		return nil, err
+	}
 
 	return nil, nil
+}
+
+func addAccount(name string, service *flowkit.Account, services *services.Services, state *flowkit.State) error {
+	pkey, err := services.Keys.Generate("", crypto.ECDSA_P256)
+	if err != nil {
+		return err
+	}
+
+	// create the account on the network and set the address
+	flowAcc, err := services.Accounts.Create(
+		service,
+		[]crypto.PublicKey{pkey.PublicKey()},
+		[]int{flow.AccountKeyWeightThreshold},
+		[]crypto.SignatureAlgorithm{crypto.ECDSA_P256},
+		[]crypto.HashAlgorithm{crypto.SHA3_256},
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	account := flowkit.NewAccount(name)
+	account.SetAddress(flowAcc.Address)
+	account.SetKey(flowkit.NewHexAccountKeyFromPrivateKey(0, crypto.SHA3_256, pkey))
+
+	state.Accounts().AddOrUpdate(account)
+	return nil
+}
+
+func addContract(path string, state *flowkit.State, readerWriter flowkit.ReaderWriter) (*config.Contract, error) {
+	content, err := readerWriter.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	program, err := project.NewProgram(flowkit.NewScript(content, nil, path))
+	if err != nil {
+		return nil, err
+	}
+
+	name, err := program.Name()
+	if err != nil {
+		return nil, err
+	}
+
+	contract := config.Contract{
+		Name:     name,
+		Location: path,
+	}
+	state.Contracts().AddOrUpdate(name, contract)
+	return &contract, nil
 }
