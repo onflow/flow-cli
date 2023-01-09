203b9ec8a993743e83741b57e952f937a25f1c93
Dylan Tinianov <dylantinianov@gmail.com>
2023-01-09 12:31:31 -0500

    Stop flow.exe before updating

--- install.ps1
+++ install.ps1
@@ -51,7 +51,10 @@ Invoke-WebRequest -Uri "$assetsURL/$version/flow-cli-$version-windows-amd64.zip"
 
 Expand-Archive -Path "$directory\flow.zip" -DestinationPath "$directory" -Force
 
-Remove-Item -Path "$directory\flow.exe" -Force
+try {
+    Stop-Process -Name flow -Force
+}
+catch {}
 
 Move-Item -Path "$directory\flow-cli.exe" -Destination "$directory\flow.exe" -Force
 
