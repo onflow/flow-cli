4943322f748fc7816cd0e1ac63d600259dc3693b
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-01-31 15:46:49 +0100

    changed error

--- pkg/flowkit/project/deployment_test.go
+++ pkg/flowkit/project/deployment_test.go
@@ -219,7 +219,7 @@ func TestContractDeploymentOrder(t *testing.T) {
 			}
 
 			if strings.Contains(testCase.name, "unresolved") {
-				assert.EqualError(t, err, "import from ContractH could not be found: Foo.cdc, make sure import path is correct")
+				assert.EqualError(t, err, "import from ContractH could not be found: Foo.cdc, make sure import path is correct, and the contract is added to deployments or has an alias")
 				return
 			}
 
