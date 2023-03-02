4bc6514f9d521b624877ad1a8f8859dfb9b40b84
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-03-02 17:57:49 +0100

    remove unimplemented functions

--- pkg/flowkit/flowkit_test.go
+++ pkg/flowkit/flowkit_test.go
@@ -172,58 +172,6 @@ func TestAccounts(t *testing.T) {
 		assert.NotNil(t, account)
 		assert.NoError(t, err)
 	})
-
-	t.Run("Staking Info for Account", func(t *testing.T) {
-		_, flowkit, gw := setup()
-
-		count := 0
-		gw.ExecuteScript.Run(func(args mock.Arguments) {
-			count++
-			assert.True(t, strings.Contains(string(args.Get(0).([]byte)), "import FlowIDTableStaking from 0x9eca2b38b18b5dfe"))
-			gw.ExecuteScript.Return(cadence.NewArray([]cadence.Value{}), nil)
-		})
-
-		val1, val2, err := flowkit.Accounts.StakingInfo(flow.HexToAddress("df9c30eb2252f1fa"))
-		assert.NoError(t, err)
-		assert.NotNil(t, val1)
-		assert.NotNil(t, val2)
-		assert.Equal(t, 2, count)
-	})
-	t.Run("Staking Info for Account fetches node total", func(t *testing.T) {
-		_, flowkit, gw := setup()
-
-		count := 0
-		gw.ExecuteScript.Run(func(args mock.Arguments) {
-			assert.True(t, strings.Contains(string(args.Get(0).([]byte)), "import FlowIDTableStaking from 0x9eca2b38b18b5dfe"))
-			if count < 2 {
-				gw.ExecuteScript.Return(cadence.NewArray(
-					[]cadence.Value{
-						cadence.Struct{
-							StructType: &cadence.StructType{
-								Fields: []cadence.Field{
-									{
-										Identifier: "id",
-									},
-								},
-							},
-							Fields: []cadence.Value{
-								cadence.String("8f4d09dae7918afbf62c48fa968a9e8b0891cee8442065fa47cc05f4bc9a8a91"),
-							},
-						},
-					}), nil)
-			} else {
-				assert.True(t, strings.Contains(args.Get(1).([]cadence.Value)[0].String(), "8f4d09dae7918afbf62c48fa968a9e8b0891cee8442065fa47cc05f4bc9a8a91"))
-				gw.ExecuteScript.Return(cadence.NewUFix64("1.0"))
-			}
-			count++
-		})
-
-		val1, val2, err := s.Accounts.StakingInfo(flow.HexToAddress("df9c30eb2252f1fa"))
-		assert.NoError(t, err)
-		assert.NotNil(t, val1)
-		assert.NotNil(t, val2)
-		assert.Equal(t, 3, count)
-	})
 }
 
 func setupIntegration() (*State, Flowkit) {
@@ -600,17 +548,6 @@ func TestAccountsGet_Integration(t *testing.T) {
 	})
 }
 
-func TestAccountsStakingInfo_Integration(t *testing.T) {
-	t.Parallel()
-	state, flowkit := setupIntegration()
-	srvAcc, _ := state.EmulatorServiceAccount()
-
-	t.Run("Get Staking Info", func(t *testing.T) {
-		_, _, err := s.Accounts.StakingInfo(srvAcc.Address()) // unfortunately can't do integration test
-		assert.Equal(t, err.Error(), "emulator chain not supported")
-	})
-}
-
 func TestBlocks(t *testing.T) {
 	t.Parallel()
 
@@ -928,75 +865,10 @@ func TestKeys(t *testing.T) {
 		}
 
 	})
-
-	t.Run("Decode RLP Key", func(t *testing.T) {
-		t.Parallel()
-
-		_, flowkit, _ := setup()
-		dkey, err := s.Keys.DecodeRLP("f847b84084d716c14b051ad6b001624f738f5d302636e6b07cc75e4530af7776a4368a2b586dbefc0564ee28384c2696f178cbed52e62811bcc9ecb59568c996d342db2402038203e8")
-
-		assert.NoError(t, err)
-		assert.Equal(t, dkey.PublicKey.String(), "0x84d716c14b051ad6b001624f738f5d302636e6b07cc75e4530af7776a4368a2b586dbefc0564ee28384c2696f178cbed52e62811bcc9ecb59568c996d342db24")
-		assert.Equal(t, dkey.SigAlgo.String(), "ECDSA_P256")
-	})
-
-	t.Run("Decode RLP Key Invalid", func(t *testing.T) {
-		t.Parallel()
-
-		_, flowkit, _ := setup()
-		_, err := s.Keys.DecodeRLP("aaa")
-		assert.Equal(t, err.Error(), "failed to decode public key: encoding/hex: odd length hex string")
-	})
-
-	t.Run("Decode PEM Key", func(t *testing.T) {
-		t.Parallel()
-
-		_, flowkit, _ := setup()
-		dkey, err := s.Keys.DecodePEM("-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE1HmzzcntvdsZXLErNRYa3oJrAypk\nvdQGLMh/s7p+ccnPZG/yOZC7RTLKRcRFx+kIzvJ4ssRhU2ADmmZgo2apXw==\n-----END PUBLIC KEY-----", crypto.ECDSA_P256)
-
-		assert.NoError(t, err)
-		assert.Equal(t, dkey.PublicKey.String(), "0xd479b3cdc9edbddb195cb12b35161ade826b032a64bdd4062cc87fb3ba7e71c9cf646ff23990bb4532ca45c445c7e908cef278b2c4615360039a6660a366a95f")
-		assert.Equal(t, dkey.SigAlgo.String(), "ECDSA_P256")
-	})
-
-	t.Run("Decode PEM Key Invalid", func(t *testing.T) {
-		t.Parallel()
-
-		_, flowkit, _ := setup()
-		_, err := s.Keys.DecodePEM("nope", crypto.ECDSA_P256)
-		assert.Equal(t, err.Error(), "crypto: failed to parse PEM string, not all bytes in PEM key were decoded: 6e6f7065")
-	})
 }
 
 func TestProject(t *testing.T) {
 	t.Parallel()
-
-	t.Run("Init Project", func(t *testing.T) {
-		t.Parallel()
-
-		st, s, _ := setup()
-		pkey := tests.PrivKeys()[0]
-		init, err := s.Project.Init(st.ReaderWriter(), false, false, crypto.ECDSA_P256, crypto.SHA3_256, pkey)
-		assert.NoError(t, err)
-
-		sacc, err := init.EmulatorServiceAccount()
-		assert.NotNil(t, sacc)
-		assert.NoError(t, err)
-		assert.Equal(t, sacc.Name(), config.DefaultEmulatorServiceAccountName)
-		assert.Equal(t, sacc.Address().String(), "f8d6e0586b0a20c7")
-
-		p, err := sacc.Key().PrivateKey()
-		assert.NoError(t, err)
-		assert.Equal(t, (*p).String(), pkey.String())
-
-		init, err = s.Project.Init(st.ReaderWriter(), false, false, crypto.ECDSA_P256, crypto.SHA3_256, nil)
-		assert.NoError(t, err)
-		em, err := init.EmulatorServiceAccount()
-		assert.NoError(t, err)
-		k, err := em.Key().PrivateKey()
-		assert.NoError(t, err)
-		assert.NotNil(t, (*k).String())
-	})
 
 	t.Run("Deploy Project", func(t *testing.T) {
 		t.Parallel()
