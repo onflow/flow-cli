f3b0ddf7eb97441438fa2f4c7d4550d959d490f8
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-03-14 13:54:46 +0100

    add tests for account create

--- internal/accounts/accounts_test.go
+++ internal/accounts/accounts_test.go
@@ -1,6 +1,7 @@
 package accounts
 
 import (
+	"fmt"
 	"github.com/onflow/flow-cli/internal/command"
 	"github.com/onflow/flow-cli/pkg/flowkit"
 	"github.com/onflow/flow-cli/pkg/flowkit/mocks"
@@ -139,5 +140,85 @@ func Test_UpdateContract(t *testing.T) {
 
 		assert.Nil(t, result)
 		assert.EqualError(t, err, "error loading contract file: open non-existing: file does not exist")
+	})
+}
+
+func Test_Create(t *testing.T) {
+	srv, state, _ := CommandWithState(t)
+
+	t.Run("Success", func(t *testing.T) {
+		pkey := "014d91eb68b5fddeca118821e74f70b48d9582c8546d8a2ae9d6835cdb7d1d008624945f55c4b409c628b63a89a54570ed028e8e68a1fe0c98ef08d7f488037b"
+		createFlags.Keys = []string{pkey}
+
+		srv.CreateAccount.Run(func(args mock.Arguments) {
+			acc := args.Get(1).(*flowkit.Account)
+			keys := args.Get(2).([]flowkit.Key)
+			assert.Equal(t, "emulator-account", acc.Name())
+			assert.Len(t, keys, 1)
+			assert.Equal(t, fmt.Sprintf("0x%s", pkey), keys[0].Public.String())
+			assert.Equal(t, crypto.ECDSA_P256, keys[0].SigAlgo)
+			assert.Equal(t, crypto.SHA3_256, keys[0].HashAlgo)
+		})
+
+		result, err := create([]string{}, NoFlags, NoLogger, srv.Mock, state)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+	})
+
+	t.Run("Success multiple keys", func(t *testing.T) {
+		pkey1 := "014d91eb68b5fddeca118821e74f70b48d9582c8546d8a2ae9d6835cdb7d1d008624945f55c4b409c628b63a89a54570ed028e8e68a1fe0c98ef08d7f488037b"
+		pkey2 := "c4bcde70e3c29cdc472ce7be46e219ca42f0ed2174369b3ba693c5655ed03f7027c571ba3881ed4b480fba41760572bcc167a8dbcf4e6ed952dcce831f82fc92"
+		createFlags.Keys = []string{pkey1, pkey2}
+		createFlags.SigAlgo = []string{"ECDSA_P256", "ECDSA_secp256k1"}
+		createFlags.HashAlgo = []string{"SHA3_256", "SHA2_256"}
+		createFlags.Weights = []int{500, 500}
+
+		srv.CreateAccount.Run(func(args mock.Arguments) {
+			acc := args.Get(1).(*flowkit.Account)
+			keys := args.Get(2).([]flowkit.Key)
+			assert.Equal(t, "emulator-account", acc.Name())
+			assert.Len(t, keys, 2)
+
+			assert.Equal(t, fmt.Sprintf("0x%s", pkey1), keys[0].Public.String())
+			assert.Equal(t, crypto.ECDSA_P256, keys[0].SigAlgo)
+			assert.Equal(t, crypto.SHA3_256, keys[0].HashAlgo)
+			assert.Equal(t, 500, keys[0].Weight)
+
+			assert.Equal(t, fmt.Sprintf("0x%s", pkey2), keys[1].Public.String())
+			assert.Equal(t, crypto.ECDSA_secp256k1, keys[1].SigAlgo)
+			assert.Equal(t, 500, keys[1].Weight)
+		})
+
+		result, err := create([]string{}, NoFlags, NoLogger, srv.Mock, state)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+	})
+
+	t.Run("Fail not enough weights", func(t *testing.T) {
+		pkey1 := "014d91eb68b5fddeca118821e74f70b48d9582c8546d8a2ae9d6835cdb7d1d008624945f55c4b409c628b63a89a54570ed028e8e68a1fe0c98ef08d7f488037b"
+		pkey2 := "c4bcde70e3c29cdc472ce7be46e219ca42f0ed2174369b3ba693c5655ed03f7027c571ba3881ed4b480fba41760572bcc167a8dbcf4e6ed952dcce831f82fc92"
+		createFlags.Keys = []string{pkey1, pkey2}
+		createFlags.SigAlgo = []string{"ECDSA_P256", "ECDSA_secp256k1"}
+		createFlags.HashAlgo = []string{"SHA3_256", "SHA2_256"}
+		createFlags.Weights = []int{1000}
+
+		result, err := create([]string{}, NoFlags, NoLogger, srv.Mock, state)
+		require.EqualError(t, err, "must provide a key weight for each key provided, keys provided: 2, weights provided: 1")
+		require.Nil(t, result)
+	})
+
+	t.Run("Fail parse keys", func(t *testing.T) {
+		_, err := parsePublicKeys([]string{"invalid"}, []crypto.SignatureAlgorithm{crypto.ECDSA_P256})
+		assert.EqualError(t, err, "failed decoding public key: invalid with error: encoding/hex: invalid byte: U+0069 'i'")
+	})
+
+	t.Run("Fail parse hash", func(t *testing.T) {
+		_, err := parseHashingAlgorithms([]string{"invalid"})
+		assert.EqualError(t, err, "invalid hash algorithm: invalid")
+	})
+
+	t.Run("Fail parse signature algorithm", func(t *testing.T) {
+		_, err := parseSignatureAlgorithms([]string{"invalid"})
+		assert.EqualError(t, err, "invalid signature algorithm: invalid")
 	})
 }
