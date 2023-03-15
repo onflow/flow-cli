1fa78b284735ff137425b0dcc4591a4e2eba8216
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-03-15 12:12:08 +0100

    add missing decode key

--- internal/keys/keys_test.go
+++ internal/keys/keys_test.go
@@ -40,6 +40,53 @@ func Test_DecodeKeys(t *testing.T) {
 		_, err := decodePEM("nope", crypto.ECDSA_P256)
 		assert.EqualError(t, err, "crypto: failed to parse PEM string, not all bytes in PEM key were decoded: 6e6f7065")
 	})
+
+	srv, _, rw := util.TestMocks(t)
+	t.Run("Success", func(t *testing.T) {
+		inArgsTests := [][]string{
+			{"rlp", "f847b84084d716c14b051ad6b001624f738f5d302636e6b07cc75e4530af7776a4368a2b586dbefc0564ee28384c2696f178cbed52e62811bcc9ecb59568c996d342db2402038203e8"},
+			{"pem", "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE1HmzzcntvdsZXLErNRYa3oJrAypk\nvdQGLMh/s7p+ccnPZG/yOZC7RTLKRcRFx+kIzvJ4ssRhU2ADmmZgo2apXw==\n-----END PUBLIC KEY-----"},
+		}
+
+		for _, inArgs := range inArgsTests {
+			result, err := decode(inArgs, util.NoFlags, util.NoLogger, rw, srv.Mock)
+			assert.NoError(t, err)
+			assert.NotNil(t, result)
+		}
+	})
+
+	t.Run("Success from file", func(t *testing.T) {
+		inArgs := []string{"rlp"}
+		_ = rw.WriteFile("test", []byte("f847b84084d716c14b051ad6b001624f738f5d302636e6b07cc75e4530af7776a4368a2b586dbefc0564ee28384c2696f178cbed52e62811bcc9ecb59568c996d342db2402038203e8"), 0677)
+		decodeFlags.FromFile = "test"
+
+		result, err := decode(inArgs, util.NoFlags, util.NoLogger, rw, srv.Mock)
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+	})
+
+	t.Run("Fail invalid args", func(t *testing.T) {
+		inArgs := []string{"invalid", "invalid"}
+		result, err := decode(inArgs, util.NoFlags, util.NoLogger, rw, srv.Mock)
+		assert.EqualError(t, err, "encoding type not supported. Valid encoding: RLP and PEM")
+		assert.Nil(t, result)
+	})
+
+	t.Run("Fail invalid args", func(t *testing.T) {
+		inArgs := []string{"", ""}
+		result, err := decode(inArgs, util.NoFlags, util.NoLogger, rw, srv.Mock)
+		assert.EqualError(t, err, "provide argument for encoded key or use from file flag")
+		assert.Nil(t, result)
+	})
+
+	t.Run("Fail invalid flags", func(t *testing.T) {
+		inArgs := []string{"rlp", "some public key"}
+		decodeFlags.FromFile = "from file"
+
+		result, err := decode(inArgs, util.NoFlags, util.NoLogger, rw, srv.Mock)
+		assert.EqualError(t, err, "can not pass both command argument and from file flag")
+		assert.Nil(t, result)
+	})
 }
 
 func Test_DeriveKeys(t *testing.T) {
