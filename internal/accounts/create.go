6bb6740897596eb4abf684cdfd5de8ac3c8ddfb4
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-03-24 14:39:26 +0100

    remove ability to create account with contracts

--- internal/accounts/create.go
+++ internal/accounts/create.go
@@ -32,13 +32,12 @@ import (
 )
 
 type flagsCreate struct {
-	Signer    string   `default:"emulator-account" flag:"signer" info:"Account name from configuration used to sign the transaction"`
-	Keys      []string `flag:"key" info:"Public keys to attach to account"`
-	Weights   []int    `default:"1000" flag:"key-weight" info:"Weight for the key"`
-	SigAlgo   []string `default:"ECDSA_P256" flag:"sig-algo" info:"Signature algorithm used to generate the keys"`
-	HashAlgo  []string `default:"SHA3_256" flag:"hash-algo" info:"Hash used for the digest"`
-	Contracts []string `flag:"contract" info:"Contract to be deployed during account creation. <name:filename>"`
-	Include   []string `default:"" flag:"include" info:"Fields to include in the output"`
+	Signer   string   `default:"emulator-account" flag:"signer" info:"Account name from configuration used to sign the transaction"`
+	Keys     []string `flag:"key" info:"Public keys to attach to account"`
+	Weights  []int    `default:"1000" flag:"key-weight" info:"Weight for the key"`
+	SigAlgo  []string `default:"ECDSA_P256" flag:"sig-algo" info:"Signature algorithm used to generate the keys"`
+	HashAlgo []string `default:"SHA3_256" flag:"hash-algo" info:"Hash used for the digest"`
+	Include  []string `default:"" flag:"include" info:"Fields to include in the output"`
 }
 
 var createFlags = flagsCreate{}
@@ -112,7 +111,6 @@ func create(
 		}
 	}
 
-	// todo if contracts are provided we should also deploy as seperate action
 	account, _, err := flow.CreateAccount(
 		context.Background(),
 		signer,
