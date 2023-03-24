7a3f33dcc5281a3f35957d8ac8f2f61e9a7a4ad9
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-03-24 12:33:30 +0100

    fix moved method

--- internal/status/status.go
+++ internal/status/status.go
@@ -25,9 +25,9 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/onflow/flow-cli/internal/command"
+	"github.com/onflow/flow-cli/internal/util"
 	"github.com/onflow/flow-cli/pkg/flowkit"
 	"github.com/onflow/flow-cli/pkg/flowkit/output"
-	"github.com/onflow/flow-cli/pkg/flowkit/util"
 )
 
 type FlagsStatus struct {
