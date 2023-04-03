d8a7fa74b03fda24feb6bbd032b5eccf4138acf5
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-04-03 15:16:05 +0200

    fix api changes

--- internal/accounts/accounts_test.go
+++ internal/accounts/accounts_test.go
@@ -42,7 +42,7 @@ func Test_AddContract(t *testing.T) {
 		inArgs := []string{tests.ContractSimpleWithArgs.Filename, "1"}
 
 		srv.AddContract.Run(func(args mock.Arguments) {
-			script := args.Get(2).(*flowkit.Script)
+			script := args.Get(2).(flowkit.Script)
 			assert.Equal(t, tests.ContractSimpleWithArgs.Filename, script.Location)
 			assert.Len(t, script.Args, 1)
 			assert.Equal(t, inArgs[1], script.Args[0].String())
@@ -56,7 +56,7 @@ func Test_AddContract(t *testing.T) {
 
 	t.Run("Success JSON arg", func(t *testing.T) {
 		srv.AddContract.Run(func(args mock.Arguments) {
-			script := args.Get(2).(*flowkit.Script)
+			script := args.Get(2).(flowkit.Script)
 			assert.Equal(t, tests.ContractSimpleWithArgs.Filename, script.Location)
 			assert.Len(t, script.Args, 1)
 			assert.Equal(t, "1", script.Args[0].String())
@@ -131,7 +131,7 @@ func Test_UpdateContract(t *testing.T) {
 		inArgs := []string{tests.ContractSimpleWithArgs.Filename, "1"}
 
 		srv.AddContract.Run(func(args mock.Arguments) {
-			script := args.Get(2).(*flowkit.Script)
+			script := args.Get(2).(flowkit.Script)
 			assert.Equal(t, tests.ContractSimpleWithArgs.Filename, script.Location)
 			assert.Len(t, script.Args, 1)
 			assert.Equal(t, inArgs[1], script.Args[0].String())
@@ -148,7 +148,7 @@ func Test_UpdateContract(t *testing.T) {
 		inArgs := []string{tests.ContractSimpleWithArgs.Filename}
 
 		srv.AddContract.Run(func(args mock.Arguments) {
-			script := args.Get(2).(*flowkit.Script)
+			script := args.Get(2).(flowkit.Script)
 			assert.Equal(t, tests.ContractSimpleWithArgs.Filename, script.Location)
 			assert.Len(t, script.Args, 1)
 			assert.Equal(t, "1", script.Args[0].String())
