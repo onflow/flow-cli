c4ce79ce385f2176592800c933b31a2061a30a2d
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-04-04 14:10:57 +0200

    add headers

--- internal/util/test.go
+++ internal/util/test.go
@@ -1,3 +1,21 @@
+/*
+ * Flow CLI
+ *
+ * Copyright 2019 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package util
 
 import (
@@ -14,6 +32,7 @@ import (
 )
 
 var NoLogger = output.NewStdoutLogger(output.NoneLog)
+
 var TestID = flow.HexToID("24993fc99f81641c45c0afa307e683b4f08d407d90041aa9439f487acb33d633")
 
 // TestMocks creates mock flowkit services, an empty state and a mock reader writer
