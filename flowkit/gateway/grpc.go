f49186ce7511607da8d88a6bbecdfaa3ff204cf4
Bjarte Stien Karlsen <bjarte@bjartek.org>
2023-08-02 14:05:23 +0200

    added new max grpc size

--- flowkit/gateway/grpc.go
+++ flowkit/gateway/grpc.go
@@ -34,9 +34,8 @@ import (
 	"github.com/onflow/flow-cli/flowkit/config"
 )
 
-// maxGRPCMessageSize 20mb, matching the value set in onflow/flow-go
-// https://github.com/onflow/flow-go/blob/master/utils/grpc/grpc.go#L5
-const maxGRPCMessageSize = 1024 * 1024 * 20
+// maxGRPCMessageSize 60mb
+const maxGRPCMessageSize = 1024 * 1024 * 60
 
 // GrpcGateway is a gateway implementation that uses the Flow Access gRPC API.
 type GrpcGateway struct {
