857ebb69d397f6dce34058abb40ac631df3a3327
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-11-28 11:04:56 -0800

    Get contracts from account

--- internal/contracts/install.go
+++ internal/contracts/install.go
@@ -1,9 +1,13 @@
 package contracts
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/onflow/flow-cli/flowkit"
 	"github.com/onflow/flow-cli/flowkit/output"
 	"github.com/onflow/flow-cli/internal/command"
+	flowsdk "github.com/onflow/flow-go-sdk"
 	"github.com/spf13/cobra"
 )
 
@@ -21,12 +25,29 @@ var installCommand = &command.Command{
 }
 
 func install(
-	_ []string,
+	args []string,
 	_ command.GlobalFlags,
 	logger output.Logger,
-	_ flowkit.Services,
+	flow flowkit.Services,
 	_ *flowkit.State,
 ) (result command.Result, err error) {
-	logger.Info("Fetching contract and dependencies...")
+	if len(args) < 1 {
+		return nil, fmt.Errorf("please specify a contract address")
+	}
+
+	address := flowsdk.HexToAddress(args[0])
+	logger.Info(fmt.Sprintf("Fetching contract and dependencies for %s", address))
+	account, err := flow.GetAccount(context.Background(), address)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("account: ", len(account.Contracts))
+
+	for name, code := range account.Contracts {
+		fmt.Println("name: ", name)
+		fmt.Println("code: ", string(code))
+	}
+
 	return nil, err
 }
