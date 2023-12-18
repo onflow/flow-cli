da301e3bf54606e16b7fa5a7bd8b61fcb7158238
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-12-18 16:26:27 +0100

    add fund method

---
+++ internal/evm/fund.cdc
@@ -0,0 +1,19 @@
+import EVM from 0xf8d6e0586b0a20c7 // todo dynamically set
+import FungibleToken from 0xee82856bf20e2aa6
+import FlowToken from 0x0ae53cb6e3f42a79
+
+transaction(address: [UInt8; 20], amount: UFix64) {
+    let fundVault: @FlowToken.Vault
+
+    prepare(signer: AuthAccount) {
+        let vaultRef = signer.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
+            ?? panic("Could not borrow reference to the owner's Vault!")
+
+        self.fundVault <- vaultRef.withdraw(amount: amount) as! @FlowToken.Vault
+    }
+
+    execute {
+        let fundAddress = EVM.EVMAddress(bytes: address)
+        fundAddress.deposit(from: <-self.fundVault)
+    }
+}
---
+++ internal/evm/fund.go
@@ -0,0 +1,81 @@
+package evm
+
+import (
+	_ "embed"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/onflow/flow-cli/flowkit"
+	"github.com/onflow/flow-cli/flowkit/output"
+	"github.com/onflow/flow-cli/internal/command"
+	"github.com/onflow/flow-cli/internal/transactions"
+)
+
+//go:embed fund.cdc
+var fundCode []byte
+
+type flagsFund struct {
+	Signer string `default:"" flag:"signer" info:"Account name from configuration used to sign the transaction as proposer, payer and suthorizer"`
+}
+
+var fundFlags = flagsFund{}
+
+var fundCommand = &command.Command{
+	Cmd: &cobra.Command{
+		Use:     "fund <EVM address> <amount>",
+		Short:   "Fund an EVM address with the provided funding amount",
+		Args:    cobra.ExactArgs(2),
+		Example: "flow evm fund 522b3294e6d06aa25ad0f1b8891242e335d3b459 1.0",
+	},
+	Flags: &fundFlags,
+	RunS:  fund,
+}
+
+// todo only for demo, super hacky now
+
+func fund(
+	args []string,
+	g command.GlobalFlags,
+	_ output.Logger,
+	flow flowkit.Services,
+	state *flowkit.State,
+) (command.Result, error) {
+	address := args[0]
+	amount := args[1]
+
+	a, err := hex.DecodeString(address)
+	if err != nil {
+		return nil, err
+	}
+
+	addressBytes := cadenceByteArrayString(a)
+
+	res, err := transactions.SendTransaction(
+		fundCode,
+		[]string{addressBytes, amount},
+		"",
+		flow,
+		state,
+		transactions.Flags{
+			Signer: deployFlags.Signer,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	txRes := res.(*transactions.TransactionResult)
+	if txRes.Result.Error != nil {
+		return nil, txRes.Result.Error
+	}
+
+	fmt.Printf("\n🔥🔥🔥🔥🔥🔥🔥 EVM Address Funded 🔥🔥🔥🔥🔥🔥🔥")
+	fmt.Printf("\n-------------------------------------------------------------\n\n")
+
+	fmt.Println("Address:       ", address)
+	fmt.Println("Funded with:   ", amount)
+
+	return nil, nil
+}
