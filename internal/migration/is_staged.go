59ae218ef66f03665b4f792ce2655b320191b835
Ian Pun <ian.pun@dapperlabs.com>
2024-02-08 14:18:00 -0800

    create isStaged logic

--- internal/migration/is_staged.go
+++ internal/migration/is_staged.go
@@ -1,7 +1,9 @@
 package migration
 
 import (
+	"bytes"
 	"fmt"
+	"text/template"
 
 	"github.com/onflow/flow-cli/internal/command"
 	"github.com/onflow/flow-cli/internal/scripts"
@@ -10,35 +12,58 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type IsStagedFlags struct {
-	Network string `default:"" flag:"network" info:"network to stage the contract on"`
-	Signer string `default:"" flag:"signer" info:"signer account to stage the contract with"`
-}
-
-var isStagedflags = IsStagedFlags{}
+var isStagedflags = scripts.Flags{}
 
 var IsStagedCommand = &command.Command{
 	Cmd: &cobra.Command{
-		Use:     "flow is-staged <CONTRACT_ADDRESS> <CONTRACT_NAME> --network",
+		Use:     "flow is-staged <CONTRACT_NAME> <CONTRACT_ADDRESS>",
 		Short:   "checks to see if the contract is staged for migration",
-		Example: `flow is-staged HelloWorld hello_world.cdc --network testnet`,
+		Example: `flow is-staged HelloWorld 0xhello`,
 		Args:    cobra.MinimumNArgs(2),
 	},
 	Flags: &isStagedflags,
-	Run:   IsStaged,
+	RunS:   isStaged,
 }
 
-func IsStaged(
+
+func isStaged(
 	args []string,
-	_ command.GlobalFlags,
+	globalFlags command.GlobalFlags,
 	_ output.Logger,
-	readerWriter flowkit.ReaderWriter,
 	flow flowkit.Services,
+	state *flowkit.State,
 ) (command.Result, error) {
-	IsStagedCode, err := readerWriter.ReadFile("./scripts/get_staged_contract_code.cdc")
+	scTempl, err := template.ParseFiles("./transactions/get_staged_contract_code.cdc")
 	if err != nil {
-		return nil, fmt.Errorf("error loading stag contract file: %w", err)
+		return nil, fmt.Errorf("error loading staging contract file: %w", err)
 	}
 
-	return scripts.SendScript(IsStagedCode, args, "", flow, scripts.Flags{})
+	// render transaction template with network
+	var txScriptBuf bytes.Buffer
+	if err := scTempl.Execute(
+		&txScriptBuf, 
+		map[string]string{
+		"MigrationContractStaging": MigrationContractStagingAddress[globalFlags.Network],
+		}); err != nil {
+		return nil, fmt.Errorf("error rendering staging contract template: %w", err)
+	}
+
+
+	contractName, contractAddress := args[0], args[1]
+
+	res, err := scripts.SendScript(
+		txScriptBuf.Bytes(),
+		[]string{
+			contractName,
+			contractAddress,
+		},
+		"",
+		flow,
+		isStagedflags,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error sending script: %w", err)
+	}
+
+	return res, nil
 }
