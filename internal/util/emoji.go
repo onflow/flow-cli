eec531dc9170e67d5fcb3a3544a91b3a47929b68
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2024-04-24 10:43:55 -0700

    Make emojis conditional based on runtime os

---
+++ internal/util/emoji.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func PrintEmoji(emoji string) string {
+	if runtime.GOOS == "windows" {
+		return ""
+	}
+
+	return emoji
+}
+
+func MessageWithEmojiPrefix(emoji string, message string) string {
+	return fmt.Sprintf("%s %s", PrintEmoji(emoji), message)
+}
