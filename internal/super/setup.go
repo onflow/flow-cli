7cddd11b2beac31bb1ff03bbcebfb9fa99c9a908
Chase Fleming <chasefleming@users.noreply.github.com>
2024-05-01 13:04:34 -0700

    Update internal/super/setup.go
    Co-authored-by: Jordan Ribbink <jribbink@users.noreply.github.com>

--- internal/super/setup.go
+++ internal/super/setup.go
@@ -108,8 +108,7 @@ func startInteractiveSetup(
 	if len(args) < 1 {
 		userInput, err := prompt.RunTextInput("Enter the name of your project", "Type your project name here...")
 		if err != nil {
-			fmt.Printf("Error running project name: %v\n", err)
-			os.Exit(1)
+			return nil, fmt.Errorf("error running project name: %v", err)
 		}
 
 		targetDir, err = getTargetDirectory(userInput)
