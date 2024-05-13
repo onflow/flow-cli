751ebbc80afe82b4ffa420d6663a98f969d0a764
Ian Pun <ian.pun@dapperlabs.com>
2024-05-13 12:13:53 -0700

    update

--- internal/migrate/validator/validator.go
+++ internal/migrate/validator/validator.go
@@ -81,37 +81,6 @@ func (m missingContractError) Error() string {
 	return builder.String()
 }
 
-type brokenContractError struct {
-	BrokenContracts []struct {
-		ContractName string
-		Address      string
-		Network      string
-		Error        string
-	}
-	LastMigrationTime *time.Time
-}
-
-func (m brokenContractError) Error() string {
-	builder := strings.Builder{}
-	builder.WriteString("some  of thse contracts seem to have failed the last emulated migration (last migration report was at ")
-	builder.WriteString(m.LastMigrationTime.Format(time.RFC3339))
-	builder.WriteString(")\n\n")
-
-	for _, contract := range m.BrokenContracts {
-		builder.WriteString(" - Account: ")
-		builder.WriteString(contract.Address)
-		builder.WriteString("\n - Contract: ")
-		builder.WriteString(contract.ContractName)
-		builder.WriteString("\n - Network: ")
-		builder.WriteString(contract.Network)
-		builder.WriteString("\n - Error: ")
-		builder.WriteString(contract.Error)
-		builder.WriteString("\n\n")
-	}
-
-	return builder.String()
-}
-
 type validator struct {
 	repoService GitHubRepositoriesService
 	state       *flowkit.State
@@ -183,6 +152,7 @@ func (v *validator) getContractUpdateStatuses(contractNames ...string) ([]Contra
 	if err != nil {
 		return nil, nil, err
 	}
+
 
 	// Get all the contract statuses from the report
 	statuses, err := v.fetchAndParseReport(report.GetPath())
