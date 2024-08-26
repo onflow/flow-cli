2e639ae646afbdfe507e48a73692ed474f8107d4
Bastian Müller <bastian@turbolent.com>
2024-08-26 10:08:26 -0700

    update copyright notice

--- internal/super/flix_test.go
+++ internal/super/flix_test.go
@@ -1,7 +1,7 @@
 /*
  * Flow CLI
  *
- * Copyright 2024 Flow Foundation, Inc.
+ * Copyright Flow Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
