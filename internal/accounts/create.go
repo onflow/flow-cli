70feb4f5fac68544f82d924577824e8d806d1545
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2024-08-29 14:54:53 -0700

    Fix accounts create

--- internal/accounts/create.go
+++ internal/accounts/create.go
@@ -63,13 +63,7 @@ func create(
 	state *flowkit.State,
 ) (command.Result, error) {
 	if len(createFlags.Keys) == 0 { // if user doesn't provide any flags go into interactive mode
-
-		if len(createFlags.SigAlgo) > 0 || globalFlags.Network != "" {
-			return nil, fmt.Errorf("You provided flags, but no key. A key is required to create an account in manual mode. Remove flags if you'd like to use interactive mode.")
-		} else {
-			return createInteractive(state)
-		}
-
+		return createInteractive(state)
 	} else {
 		return createManual(state, flow)
 	}
