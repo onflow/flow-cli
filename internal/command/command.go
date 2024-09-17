15cb2ba69cd68b54b68fe357fa62afd3e99abf50
Jordan Ribbink <jribbink@users.noreply.github.com>
2024-09-17 12:34:25 -0700

    Use semver comparison for new version check (#1749)

--- internal/command/command.go
+++ internal/command/command.go
@@ -36,6 +36,7 @@ import (
 
 	"github.com/onflow/flow-cli/internal/prompt"
 
+	"github.com/coreos/go-semver/semver"
 	"github.com/dukex/mixpanel"
 	"github.com/getsentry/sentry-go"
 	"github.com/spf13/afero"
@@ -302,7 +303,18 @@ func checkVersion(logger output.Logger) {
 
 	latestVersion := fmt.Sprintf("v%s", strings.TrimPrefix(latestVersionRaw, "v"))
 
-	if currentVersion != latestVersion {
+	// compare semver versions
+	currentSemver, err := semver.NewVersion(strings.TrimPrefix(currentVersion, "v"))
+	if err != nil {
+		return
+	}
+
+	latestSemver, err := semver.NewVersion(strings.TrimPrefix(latestVersion, "v"))
+	if err != nil {
+		return
+	}
+
+	if currentSemver.LessThan(*latestSemver) {
 		logger.Info(fmt.Sprintf(
 			"\n%s  Version warning: a new version of Flow CLI is available (%s).\n"+
 				"   Read the installation guide for upgrade instructions: https://docs.onflow.org/flow-cli/install\n",
