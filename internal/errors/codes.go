cfb8d125a5767e60fe8a74f5cff57493424f15c8
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-09-15 13:13:05 -0700

    Create codes package

---
+++ internal/errors/codes.go
@@ -0,0 +1,44 @@
+/*
+ * Flow CLI
+ *
+ * Copyright Flow Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package errors
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Flow error codes
+const (
+	StorageLimitExceeded = 1103
+)
+
+// HasErrorCode checks if an error contains a specific Flow error code
+func HasErrorCode(err error, code int) bool {
+	if err == nil {
+		return false
+	}
+
+	errorCodePattern := fmt.Sprintf("[Error Code: %d]", code)
+	return strings.Contains(err.Error(), errorCodePattern)
+}
+
+// IsStorageLimitError checks if an error is a storage limit exceeded error
+func IsStorageLimitError(err error) bool {
+	return HasErrorCode(err, StorageLimitExceeded)
+}
