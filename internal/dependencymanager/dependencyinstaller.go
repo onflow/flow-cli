7a3c6534dbb71a90d9788e791825433b77f17689
Jordan Ribbink <jribbink@telus.net>
2025-11-25 14:46:33 -0800

    Skip updating dependencies aliased on another network

--- internal/dependencymanager/dependencyinstaller.go
+++ internal/dependencymanager/dependencyinstaller.go
@@ -577,6 +577,9 @@ func (di *DependencyInstaller) handleFoundContract(dependency config.Dependency,
 			os.Exit(0)
 			return nil
 		}
+		
+		// Alias matched - contract already stored, encountered via different network. Skip state update.
+		return nil
 	}
 
 	// Check if remote source version is different from local version
