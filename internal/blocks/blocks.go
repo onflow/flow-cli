f2fd5c52400493258b6dfecf0ed2e5e07bfef4ba
Jordan Ribbink <jribbink@telus.net>
2025-12-01 13:53:37 -0800

    Include system transactions in `flow blocks get <txid>`

--- internal/blocks/blocks.go
+++ internal/blocks/blocks.go
@@ -42,10 +42,11 @@ func init() {
 }
 
 type blockResult struct {
-	block       *flow.Block
-	events      []flow.BlockEvents
-	collections []*flow.Collection
-	included    []string
+	block        *flow.Block
+	events       []flow.BlockEvents
+	transactions []*flow.Transaction
+	results      []*flow.TransactionResult
+	included     []string
 }
 
 func (r *blockResult) JSON() any {
@@ -56,23 +57,36 @@ func (r *blockResult) JSON() any {
 	result["totalSeals"] = len(r.block.Seals)
 	result["totalCollections"] = len(r.block.CollectionGuarantees)
 
+	// Keep collection info for backwards compatibility
 	collections := make([]any, 0, len(r.block.CollectionGuarantees))
-	for i, guarantee := range r.block.CollectionGuarantees {
+	for _, guarantee := range r.block.CollectionGuarantees {
 		collection := make(map[string]any)
 		collection["id"] = guarantee.CollectionID.String()
-
-		if command.ContainsFlag(r.included, "transactions") {
-			txs := make([]string, 0)
-			for _, tx := range r.collections[i].TransactionIDs {
-				txs = append(txs, tx.String())
-			}
-			collection["transactions"] = txs
-		}
-
 		collections = append(collections, collection)
 	}
+	result["collections"] = collections
 
-	result["collection"] = collections
+	// Add transaction details if requested
+	if command.ContainsFlag(r.included, "transactions") && len(r.transactions) > 0 {
+		txs := make([]map[string]any, 0, len(r.transactions))
+		for i, tx := range r.transactions {
+			txData := make(map[string]any)
+			txData["id"] = tx.ID().String()
+			txData["status"] = r.results[i].Status.String()
+
+			// System transactions have empty collection ID
+			if r.results[i].CollectionID == flow.EmptyID {
+				txData["type"] = "system"
+			} else {
+				txData["type"] = "user"
+				txData["collectionId"] = r.results[i].CollectionID.String()
+			}
+
+			txs = append(txs, txData)
+		}
+		result["transactions"] = txs
+	}
+
 	return result
 }
 
@@ -99,17 +113,40 @@ func (r *blockResult) String() string {
 	_, _ = fmt.Fprintf(writer, "Status\t%s\n", blockStatusToString(r.block.Status))
 
 	_, _ = fmt.Fprintf(writer, "Total Seals\t%v\n", len(r.block.Seals))
-
 	_, _ = fmt.Fprintf(writer, "Total Collections\t%v\n", len(r.block.CollectionGuarantees))
 
+	// Show collections
 	for i, guarantee := range r.block.CollectionGuarantees {
 		_, _ = fmt.Fprintf(writer, "    Collection %d:\t%s\n", i, guarantee.CollectionID)
+	}
 
-		if command.ContainsFlag(r.included, "transactions") {
-			for x, tx := range r.collections[i].TransactionIDs {
-				_, _ = fmt.Fprintf(writer, "         Transaction %d: %s\n", x, tx)
+	// Show transactions if included
+	if command.ContainsFlag(r.included, "transactions") && len(r.transactions) > 0 {
+		_, _ = fmt.Fprintf(writer, "\nTransactions:\n")
+
+		userCount := 0
+		systemCount := 0
+
+		for i, tx := range r.transactions {
+			var txType string
+			if r.results[i].CollectionID == flow.EmptyID {
+				txType = "system"
+				systemCount++
+			} else {
+				txType = "user"
+				userCount++
 			}
+
+			_, _ = fmt.Fprintf(writer, "    [%d] %s\t%s (%s)\n",
+				i,
+				tx.ID().String(),
+				r.results[i].Status.String(),
+				txType,
+			)
 		}
+
+		_, _ = fmt.Fprintf(writer, "\nTotal: %d transactions (%d user, %d system)\n",
+			len(r.transactions), userCount, systemCount)
 	}
 
 	if len(r.events) > 0 {
